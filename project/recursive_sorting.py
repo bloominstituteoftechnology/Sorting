7137f58715bde3e518cde389f84b025c6ff37e26
Elissa <elissathomas@Elissas-MacBook-Pro.local>
2019-01-14 13:00:11 -0800

    Updating recursive_sorting tests

--- project/recursive_sorting.py
+++ project/recursive_sorting.py
@@ -1,22 +1,22 @@
 ### helper function
 def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
-    merged_arr = [ ]
+    merged_arr = [0] * elements
     a = 0
     b = 0
     # since arrA and arrB already sorted, we only need to compare the first element of each when merging!
     for i in range( 0, elements ):
         if a >= len(arrA):    # all elements in arrA have been merged
-            merged_arr.append(arrB[b])
+            merged_arr[i] = arrB[b]
             b += 1
         elif b >= len(arrB):  # all elements in arrB have been merged
-            merged_arr.append(arrA[a])
+            merged_arr[i] = arrA[a]
             a += 1
         elif arrA[a] < arrB[b]:  # next element in arrA smaller, so add to final array
-            merged_arr.append(arrA[a])
+            merged_arr[i] = arrA[a]
             a += 1
         else:  # else, next element in arrB must be smaller, so add it to final array
-            merged_arr.append(arrB[b])
+            merged_arr[i] = arrB[b]
             b += 1
     return merged_arr
 
@@ -31,19 +31,25 @@ def merge_sort( arr ):
 
 
 # STRETCH: implement an in-place merge sort algorithm
-def merge_sort_in_place(arr):
+def merge_in_place(arr, start, mid, end):
+    # TO-DO
+
+    return arr
+
+def merge_sort_in_place(arr, l, r): 
     # TO-DO
 
     return arr
 
 
 # TO-DO: implement the Quick Sort function below
-def quick_sort( arr ):
+def quick_sort( arr, low, high ):
 
     return arr
 
 
 # STRETCH: implement the Timsort function below
+# hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
 def timsort( arr ):
 
     return arr
