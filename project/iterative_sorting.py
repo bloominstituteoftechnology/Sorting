84e318b0f099a76679d3db8ed717325e61d4af6b
Jeanette <jeanfern555@gmail.com>
2019-01-16 17:00:54 -0600

    Completed a working solution for insertion_sort in iterative_sorting.py.

--- project/iterative_sorting.py
+++ project/iterative_sorting.py
@@ -20,8 +20,35 @@ def selection_sort( arr ):
 
 # TO-DO: implement the Insertion Sort function below
 def insertion_sort( arr ):
+    i = 1
+    while i < len(arr):
+        current_num = arr[i]
+        j = i-1
+        while j >= 0 and arr[j] > current_num:
+            arr[j+1] = arr[j]
+            j -= 1
+
+        arr[j+1] = current_num
+        i += 1
+
+    # # First Attempt:
+    # for i in range(1, len(arr)):
+    #     current_num = arr[i]
+    #     index = i
+
+    #     while index > 0 and arr[index-1] > current_num:
+    #         arr[index] = arr[index-1]
+    #         index -= 1
+
+    #     arr[index] == current_num
 
     return arr
+
+arr = [2,5,9,7,4,1,3,8,6]
+print(arr)
+arr = insertion_sort( arr )
+print(arr)
+
 
 
 # STRETCH: implement the Bubble Sort function below
