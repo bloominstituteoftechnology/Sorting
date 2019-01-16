fb992eb4720186f378dd6627ac84a825774d2379
Drew <drewsup123c@gmail.com>
2019-01-16 13:29:35 -0700

    finished selection sort in iterative sorting.y

--- project/iterative_sorting.py
+++ project/iterative_sorting.py
@@ -6,16 +6,17 @@ def selection_sort( arr ):
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-             
 
-
-
-        # TO-DO: swap
-
-
-
-
+        for j in range(i + 1, len(arr)):
+            if arr[smallest_index] > arr[j]:
+                # Swap the found minimum element with the first element
+                # if it is smaller than current smallest_index
+                smallest_index = j
+                # switches index value not the actual number itself
+        #  This swaps initial value (first value) with new smallest value
+        arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
     return arr
+print(selection_sort([2,5,1,7,8]))
 
 
 # TO-DO: implement the Insertion Sort function below
