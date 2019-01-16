7a173a324f405677381ec150c580e866ea7e77f0
jmbradley <jmbradley808@gmail.com>
2019-01-16 18:39:44 -0500

    bubble

--- project/iterative_sorting.py
+++ project/iterative_sorting.py
@@ -20,21 +20,21 @@ def selection_sort(arr):
 # TO-DO: implement the Insertion Sort function below
 def insertion_sort(arr):
     # utilizing swaps
-    # for i in range(1, len(arr)):
-    #     for j in range(i-1, 0, -1):
-    #         if arr[j] > arr[j+i]:
-    #             arr[j], arr[j+1] = arr[j+1], arr[j]
-    #         else:
-    #             break
-    # utilizing shifts
-        for i in range(1, len(arr)):
-         currNum = arr[i]
+    for i in range(1, len(arr)):
         for j in range(i-1, 0, -1):
-            if arr[j] > currNum:
-                arr[j+1] = arr[j]
+            if arr[j] > arr[j+i]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
             else:
-                arr[j+1] = currNum
                 break
+    # utilizing shifts
+        # for i in range(1, len(arr)):
+        #  currNum = arr[i]
+        # for j in range(i-1, 0, -1):
+        #     if arr[j] > currNum:
+        #         arr[j+1] = arr[j]
+        #     else:
+        #         arr[j+1] = currNum
+        #         break
 
     # for i in range(1, len(arr)):
     #     j = i -1
@@ -47,7 +47,10 @@ def insertion_sort(arr):
 
 # STRETCH: implement the Bubble Sort function below
 def bubble_sort(arr):
-
+    for i in range(0, len(arr) - 1):
+        for j in range(0, len(arr) - 1 - i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j+1] = arr[j + 1], arr[j]
     return arr
 
 
