227df8b2668fea3d77a04e3de6b0724dcb7a35b9
Hunter315 <35641380+Hunter315@users.noreply.github.com>
2019-01-15 20:42:29 -0700

    fixed typo in insertion sort

--- project/iterative_sorting.py
+++ project/iterative_sorting.py
@@ -21,7 +21,7 @@ def selection_sort( arr ):
 # TO-DO: implement the Insertion Sort function below
 def insertion_sort( arr ):
     for i in range(1, len(arr)):
-        key = i
+        key = arr[i]
         j = i - 1
         while j >= 0 and key < arr[j]:
             arr[j+1] = arr[j]
