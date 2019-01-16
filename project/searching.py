31ecc134923d79249728952661ccbcbd4a9d9df0
Linh Bouniol <paulalinh89@gmail.com>
2019-01-16 14:41:12 -0800

    Completed searching and ran test

--- project/searching.py
+++ project/searching.py
@@ -1,9 +1,11 @@
 # STRETCH: implement Linear Search				
 def linear_search(arr, target):
   
-  # TO-DO: add missing code
-
-   return -1   # not found
+  for element in range(len(arr)):
+    if arr[element] == target:
+      return element
+  
+  return -1   # not found
 
 
 # STRETCH: write an iterative implementation of Binary Search 
@@ -15,16 +17,30 @@ def binary_search(arr, target):
   low = 0
   high = len(arr)-1
 
-  # TO-DO: add missing code
-
+  while low <= high:
+    middle = (low + high) // 2
+    
+    if arr[middle] == target:
+      return middle
+    elif arr[middle] > target:  # If target is less than the middle, ignore the right side
+      high = middle - 1
+    else:
+      low = middle + 1
+    
   return -1 # not found
 
 
 # STRETCH: write a recursive implementation of Binary Search 
 def binary_search_recursive(arr, target, low, high):
   
-  middle = (low+high)/2
+  middle = (low + high) // 2
 
   if len(arr) == 0:
     return -1 # array empty
-  # TO-DO: add missing if/else statements, recursive calls
+  
+  if arr[middle] == target:
+    return middle
+  elif arr[middle] > target:
+    return binary_search_recursive(arr, target, low, middle - 1)
+  else:
+    return binary_search_recursive(arr, target, middle + 1, high)
