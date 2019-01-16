189064027efbfb5100c71fa8458b1f1a276b2769
Vuk Radosavljevic <vukrado1@gmail.com>
2019-01-16 17:55:07 -0600

    completed binary search recursive

--- project/searching.py
+++ project/searching.py
@@ -41,8 +41,15 @@ print(binary_search([1,2,3,4,5,6,7,8,9,10], 9))
 # STRETCH: write a recursive implementation of Binary Search 
 def binary_search_recursive(arr, target, low, high):
   
-  middle = (low+high)/2
+  middle = (low+high)//2
 
   if len(arr) == 0:
     return -1 # array empty
   # TO-DO: add missing if/else statements, recursive calls
+  if arr[middle] == target:
+    return middle
+  else:
+    if arr[middle] < target:
+      return binary_search_recursive(arr, target, middle, high)
+    else:
+      return binary_search_recursive(arr, target, low, middle)
