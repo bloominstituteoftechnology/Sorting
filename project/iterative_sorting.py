7a69a3436104ecf1381639b4caa9f8df4b44c585
Katie Cleary <katiecleary@airmaster.lan>
2019-01-16 16:06:13 -0500

    insertion sort completed

--- project/iterative_sorting.py
+++ project/iterative_sorting.py
@@ -23,6 +23,15 @@ def selection_sort( arr ):
 
 # TO-DO: implement the Insertion Sort function below
 def insertion_sort( arr ):
+    
+    for i in range(0, len(arr)):
+        key = arr[i] 
+        j = i-1
+        while j >=0 and key < arr[j]:
+            arr[j+1] = arr[j] 
+            j -= 1
+        
+        arr[j+1] = key 
 
     return arr
 
