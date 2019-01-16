555518da5be704a4893828ead91dc702ebba0d8a
Patrick Thompson <patrick.jthompson93@gmail.com>
2019-01-16 12:58:48 -0800

    finishes insertion sort

--- project/iterative_sorting.py
+++ project/iterative_sorting.py
@@ -31,7 +31,11 @@ def selection_sort(arr):
 
 
 def insertion_sort(arr):
-
+    for i in range(1, len(arr)):
+        j = i
+        while j > 0 and arr[j - 1] > arr[j]:
+            arr[j - 1], arr[j] = arr[j], arr[j - 1]
+            j -= 1
     return arr
 
 
