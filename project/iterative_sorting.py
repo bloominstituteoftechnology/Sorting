2ede9425418f0c8df0303752b39ca76d4a9b28ba
Scott Bren <smbren@ncsu.edu>
2019-01-16 16:32:14 -0500

    selection sort finished

--- project/iterative_sorting.py
+++ project/iterative_sorting.py
@@ -6,11 +6,15 @@ def selection_sort( arr ):
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-             
+        for x in range (cur_index + 1, len(arr)):
+            if arr(x) < arr(smallest_index):
+                smallest_index = x
 
 
 
         # TO-DO: swap
+        if smallest_index != cur_index:
+            arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]
 
 
 
