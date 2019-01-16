13a49d5d9ba1b0f6dc731794df8258dc9828ef49
Kat Fries <katherynejf@gmail.com>
2019-01-16 18:16:04 -0500

    insert sort done

--- NOTES.md
+++ NOTES.md
@@ -1,3 +1,7 @@
 # selection_sort
   Traverse entire array and find smallest value
-  Add value to the front of the array
+  Swap current value for the smallest
+
+# insertion_sort
+  start at the beginning of the array
+  compare first value to second value and swap if needed
--- project/iterative_sorting.py
+++ project/iterative_sorting.py
@@ -13,24 +13,43 @@ def selection_sort( arr ):
                 smallest_index = j
 
         # TO-DO: swap
+        print(arr)
         arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]
 
     return arr
-# print(selection_sort([1, 15, 8, 4, 2, 9, 6, 0, 3, 7]))
+# print(selection_sort([1, 5, 8, 4, 2, 9, 6, 0, 3, 7]))
 
 
 
 # TO-DO: implement the Insertion Sort function below
 def insertion_sort( arr ):
+    for i in range(0, len(arr) - 1):
+        left = arr[0:i+1]
+        val = arr[i + 1]
+        if val < left[-1:][0]:
+            for ind, j in enumerate(left):
+
+                if val < j:
+                    arr[i + 1], arr[ind] = arr[ind], arr[i + 1]
 
     return arr
 
+# print(selection_sort([1, 5, 8, 4, 2, 9, 6, 0, 3, 7]))
+print('*' * 10)
+# print(insertion_sort([1, 5, 8, 4, 2, 9, 6, 0, 3, 7]))
 
 # STRETCH: implement the Bubble Sort function below
 def bubble_sort( arr ):
 
+    for i in range(0, len(arr) - 1):
+        curr_val = arr[i]
+        next_val = arr[i + 1]
+        print(curr_val, next_val)
+        if curr_val > next_val:
+            arr[curr_val], arr[next_val] = arr[curr_val], arr[next_val]
+    print(arr)
     return arr
-
+print(bubble_sort([1, 5, 8, 4, 2, 9, 6, 0, 3, 7]))
 
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
