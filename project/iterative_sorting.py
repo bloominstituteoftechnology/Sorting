abf44eebe70d44a7db2da53e649af7526735f9a1
Brock Maraee Rohloff <00AgentB@gmail.com>
2019-01-16 15:47:21 -0700

    Finish Iterative version of insertion_sort

--- project/iterative_sorting.py
+++ project/iterative_sorting.py
@@ -2,28 +2,50 @@
 def selection_sort( arr ):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
+        current_index = i
+        smallest_index = current_index
+        # print(arr[i])
+        # print(arr)
 
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-        for j in range(cur_index, len(arr)):
-            if arr[j] < arr[smallest_index]:
-                smallest_index = j
+        # Find Smallest
+        for comparison_index in range(current_index, len(arr)):
+            print(arr[comparison_index])
+            if arr[comparison_index] < arr[smallest_index]:
+                smallest_index = comparison_index
             
-        # TO-DO: swap
+        # Swap
         temp = arr[smallest_index]
-        arr[smallest_index] = arr[cur_index]
-        arr[cur_index] = temp
+        arr[smallest_index] = arr[current_index]
+        arr[current_index] = temp
     
     return arr
 
-print(selection_sort([5,34,2,5,7,3,9,10]))
+# print(selection_sort([5,34,2,5,7,3,9,10]))
 
 # TO-DO: implement the Insertion Sort function below
-def insertion_sort( arr ):
 
+# Iterative
+def insertion_sort( arr ):
+    current_index = 1 # Start with the second item in the array
+    while current_index < len(arr): # Go through each position in the array 
+        cur_item = arr[current_index] # Get the current item to compare
+        comparison_index = current_index - 1 # Get the index of the item left of the current item
+        # print('loop:' + str(current_index))
+        # print(cur_item)
+
+        # Shift
+        while comparison_index >= 0 and arr[comparison_index] > cur_item: # Shift items until the beginning of the array is reached, or a smaller item is found.
+            # print(arr)
+            arr[comparison_index+1] = arr[comparison_index] # Shift the item currently being compared to with the item right of it.
+            comparison_index -= 1 # Next Loop, compare to the item left of the item currently being compared.
+
+        # Insert
+        arr[comparison_index+1] = cur_item # Insert the item into the original position of the last item that was shifted.
+        current_index += 1 # Next Loop, check the item right of the original position of the item that was just checked.
+        
     return arr
+
+print(insertion_sort([5,34,2,5,7,3,9,10]))
 
 
 # STRETCH: implement the Bubble Sort function below
