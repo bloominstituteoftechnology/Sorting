13ce8aac4dd67574eb2a4e0d7afce153bd0c0901
Brandon Strellis <bstrellis@gmail.com>
2019-01-16 17:54:43 -0500

    Correction: selection sort done.

--- project/iterative_sorting.py
+++ project/iterative_sorting.py
@@ -2,20 +2,21 @@
 def selection_sort( arr ):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
-        first_unsorted_num = arr[i]
+        index_of_min = i
 
-        for j in range(i+1, len(arr)-1):
-            compared_num = arr[j]
+        for j in range(i+1, len(arr)):
+            if arr[index_of_min] > arr[j]:
+                index_of_min = j
 
-            if compared_num < first_unsorted_num:
-                arr[i] = compared_num
-                arr[j] = first_unsorted_num
+        temp = arr[i]
+        arr[i] = arr[index_of_min]
+        arr[index_of_min] = temp
 
     return arr
 
 
 
-
+print(selection_sort([4, 3, 0, 2, 9, -7, 8]))
 
 
 
