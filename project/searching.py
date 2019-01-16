c8ffc63afa86515b26e81301c740953d6bce1270
lndubose <lndubose13@gmail.com>
2019-01-16 16:06:18 -0600

    Halfway through searching

--- project/searching.py
+++ project/searching.py
@@ -1,30 +1,47 @@
-# STRETCH: implement Linear Search				
+# STRETCH: implement Linear Search
 def linear_search(arr, target):
-  
-  # TO-DO: add missing code
+    # TO-DO: add missing code
+    for i in arr:
+        if i == target:
+            return 1
 
-   return -1   # not found
+    return -1   # not found
 
 
-# STRETCH: write an iterative implementation of Binary Search 
+print(linear_search([1, 2, 3], 3))
+print(linear_search([1, 2, 3], 4))
+# STRETCH: write an iterative implementation of Binary Search
+
+
 def binary_search(arr, target):
 
-  if len(arr) == 0:
-    return -1 # array empty
-    
-  low = 0
-  high = len(arr)-1
+    if len(arr) == 0:
+        return -1  # array empty
 
-  # TO-DO: add missing code
+    low = 0
+    high = len(arr)-1
 
-  return -1 # not found
+    # TO-DO: add missing code
+    while high >= low:
+
+        middle = round((high+low)/2)
+        if arr[middle] == target:
+            return arr[middle]
+        elif arr[middle] > target:
+            high = middle + 1
+        elif arr[middle] < target:
+            low = middle + 1
+    return -1  # not found
 
 
-# STRETCH: write a recursive implementation of Binary Search 
+print(binary_search([1, 2, 3, 4, 5, 6], 50))
+# STRETCH: write a recursive implementation of Binary Search
+
+
 def binary_search_recursive(arr, target, low, high):
-  
-  middle = (low+high)/2
 
-  if len(arr) == 0:
-    return -1 # array empty
-  # TO-DO: add missing if/else statements, recursive calls
+    middle = (low+high)/2
+
+    if len(arr) == 0:
+        return -1  # array empty
+    # TO-DO: add missing if/else statements, recursive calls
