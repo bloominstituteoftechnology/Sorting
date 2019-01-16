3f1a4f3e67f54224a291f00f420d9c59dabd56b2
luiscmartinez <mrluismartinezzz@gmail.com>
2019-01-16 14:59:27 -0800

    completed iselection_sort fun

--- project/iterative_sorting.py
+++ project/iterative_sorting.py
@@ -1,7 +1,22 @@
 # Complete the selection_sort() function below in class with your instructor
 def selection_sort( arr ):
     # loop through n-1 elements
-    
+    for i in range(0, len(arr) - 1):
+        cur_index = i
+        smallest_index = cur_index
+
+        # TO-DO: find next smallest element
+        # (hint, can do in 3 loc) 
+        for j in range(cur_index, len(arr)):
+          if arr[j] < arr[smallest_index]:
+            smallest_index = arr[j]
+
+
+        # TO-DO: swap
+    temp = arr[smallest_index]
+    arr[smallest_index] = arr[cur_index]
+    arr[cur_index] = temp
+
 
     return arr
 
