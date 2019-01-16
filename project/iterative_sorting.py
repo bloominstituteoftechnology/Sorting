2d6fa987880b144b4336336bd6d0c650ea0fe960
alejandrok93 <alejandrok93@gmail.com>
2019-01-16 15:21:47 -0800

    Searching exercises done, sorting WIP

--- project/iterative_sorting.py
+++ project/iterative_sorting.py
@@ -1,28 +1,46 @@
 # Complete the selection_sort() function below in class with your instructor
-def selection_sort( arr ):
+def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
+        # find next smallest element
+        for j in range(cur_index, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
 
-
-
-        # TO-DO: swap
-
-
-
+        # swap
+        temp = arr[smallest_index]
+        arr[smallest_index] = arr[cur_index]
+        arr[cur_index] = temp
 
     return arr
+
+
+# my_arr = [2,5,9,7,4,1,3,8,6]
+# print(my_arr)
+# arr = selection_sort(my_arr)
+# print(my_arr)
+
 
 
 # TO-DO: implement the Insertion Sort function below
 def insertion_sort( arr ):
+    for i in range(1, len(arr)-1):
+        tmp = arr[i]
+        for j in range(i, 0, -1):
+            if arr[j] < arr[i]:
+                arr[i] = tmp
+            else:
+                arr[i] = tmp
+
 
     return arr
-
+print('insertion sort:')
+my_arr = [2,5,9,7,4,1,3,8,6]
+print(my_arr)
+arr = insertion_sort(my_arr)
+print(my_arr)
 
 # STRETCH: implement the Bubble Sort function below
 def bubble_sort( arr ):
