d340688419a7b835dbe968d40818f4def54fb38d
jmbradley <jmbradley808@gmail.com>
2019-01-16 18:20:41 -0500

    sorting

--- project/iterative_sorting.py
+++ project/iterative_sorting.py
@@ -19,6 +19,28 @@ def selection_sort(arr):
 
 # TO-DO: implement the Insertion Sort function below
 def insertion_sort(arr):
+    # utilizing swaps
+    # for i in range(1, len(arr)):
+    #     for j in range(i-1, 0, -1):
+    #         if arr[j] > arr[j+i]:
+    #             arr[j], arr[j+1] = arr[j+1], arr[j]
+    #         else:
+    #             break
+    # utilizing shifts
+        for i in range(1, len(arr)):
+         currNum = arr[i]
+        for j in range(i-1, 0, -1):
+            if arr[j] > currNum:
+                arr[j+1] = arr[j]
+            else:
+                arr[j+1] = currNum
+                break
+
+    # for i in range(1, len(arr)):
+    #     j = i -1
+    #     while arr[j] > arr[j +1] and j >= 0:
+    #         arr[j], arr[j+1] = arr[j+1], arr[j]
+    #         j-= 1
 
     return arr
 
