5fa1852c3e8e19b8165c241a0a3dd74d73086779
Brock Maraee Rohloff <00AgentB@gmail.com>
2019-01-16 14:02:28 -0700

    Finish selection_sort in iterative_sorting.py

--- project/iterative_sorting.py
+++ project/iterative_sorting.py
@@ -4,19 +4,21 @@ def selection_sort( arr ):
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
+
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-             
-
-
-
+        for j in range(cur_index, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
+            
         # TO-DO: swap
-
-
-
-
+        temp = arr[smallest_index]
+        arr[smallest_index] = arr[cur_index]
+        arr[cur_index] = temp
+    
     return arr
 
+print(selection_sort([5,34,2,5,7,3,9,10]))
 
 # TO-DO: implement the Insertion Sort function below
 def insertion_sort( arr ):
