8532a270b3b13da8d1c887e2dde3f878914aefa8
Kevin <brackkevin@gmail.com>
2019-01-16 17:48:16 -0600

    bubble_sort complete, count_sort in progress

--- project/iterative_sorting.py
+++ project/iterative_sorting.py
@@ -35,10 +35,34 @@ print(insertion_sort([1,5,6,2,3]))
 # STRETCH: implement the Bubble Sort function below
 def bubble_sort( arr ):
 
+    for i in range(0, len(arr) - 1):
+        swapped = False
+
+        for j in range(0, len(arr) - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+
+        if swapped == False:
+            break
+
     return arr
+
+print(bubble_sort([1,5,6,2,3]))
 
 
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
 
+    # create a new array the size of largest - smallest filled with 0's
+    index = [0 for i in range(0, max(arr) + 1)]
+
+    # loop over existing arr and the index 
+    # of the ammount in the new_arr += 1
+    for i in range(0, len(arr) - 1):
+        index[arr[i]] += 1
+
+    
     return arr
+
+print(count_sort([1,5,1,6,2,5,3]))
