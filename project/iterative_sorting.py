a0f2932565df011828394d7f2754ea54b69967c9
lndubose <lndubose13@gmail.com>
2019-01-17 11:32:22 -0600

    Made new solution for insertion sort
    Was able to create insertion sort in another way.

--- project/iterative_sorting.py
+++ project/iterative_sorting.py
@@ -19,18 +19,39 @@ def selection_sort(arr):
 
 
 def insertion_sort(arr):
-    i = 1
+    # i = 1
 
-    while i < len(arr):
-        j = i
+    # while i < len(arr):
+    #     j = i
 
-        while j > 0 and arr[j-1] > arr[j]:
-            arr[j], arr[j-1] = arr[j-1], arr[j]
-            j -= 1
-        i += 1
-    return arr
+    #     while j > 0 and arr[j-1] > arr[j]:
+    #         arr[j], arr[j-1] = arr[j-1], arr[j]
+    #         j -= 1
+    #     i += 1
+    # return arr
+    if len(arr) == 0:
+        return arr
+
+    sortedList = [arr[0]]
+    unsorted = arr[1:]
+
+    while len(unsorted) > 0:
+        for num in sortedList[::-1]:
+            if len(unsorted) == 0:
+                return sortedList
+            if num < unsorted[0]:
+
+                sortedList.insert(sortedList.index(num) + 1, unsorted[0])
+                unsorted.pop(0)
+                break
+            elif sortedList.index(num) == 0:
+                sortedList.insert(0, unsorted[0])
+                unsorted.pop(0)
+
+    return sortedList
 
 
+print(insertion_sort([1, 5, 8, 4, 2, 9, 6, 0, 3, 7]))
 # STRETCH: implement the Bubble Sort function below
 
 
