ecb5fe914f82cfb72659bef0e74086abb1a6b308
Brock Maraee Rohloff <00AgentB@gmail.com>
2019-01-16 17:51:38 -0700

    Finish Binary Search Stretch (searching.py)

--- project/searching.py
+++ project/searching.py
@@ -5,10 +5,9 @@ def linear_search(arr, target):
   for i, j in enumerate(arr):
     if j == target:
       return i
-
   return -1   # not found
 
-print(linear_search([5,34,2,5,7,3,9,10], 9))
+# print(linear_search([5,34,2,5,7,3,9,10], 9))
 
 # STRETCH: write an iterative implementation of Binary Search 
 def binary_search(arr, target):
@@ -20,9 +19,18 @@ def binary_search(arr, target):
   high = len(arr)-1
 
   # TO-DO: add missing code
+  while high - low > 1:
+    middle = low + high // 2
+    if middle == target:
+      return middle
+    elif middle < target:
+      low = middle
+    else:
+      high = middle
 
   return -1 # not found
 
+print(linear_search([1, 2, 3, 4, 5, 7, 9, 10, 18, 19], 9))
 
 # STRETCH: write a recursive implementation of Binary Search 
 def binary_search_recursive(arr, target, low, high):
