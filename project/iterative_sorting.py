ab06afa8cd4f1db22077092172c885950729f3b2
Daniel Ferrer <danfrrr13@gmail.com>
2019-01-17 17:33:53 -0500

    Completed selection_sort and test passed.

--- project/iterative_sorting.py
+++ project/iterative_sorting.py
@@ -6,13 +6,16 @@ def selection_sort( arr ):
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-             
+        for j in range(i+1, len(arr)):
+            if arr[smallest_index] > arr[j]:
+                smallest_index = j
+       
 
 
 
         # TO-DO: swap
-
-
+    
+        arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
 
 
     return arr
--- project/tests.py
+++ project/tests.py
@@ -15,14 +15,14 @@ class SortingTest(unittest.TestCase):
     #     self.assertEqual(linear_search(arr2, 3), -1)
 
 
-    def test_binary_search(self):
-        arr1 = [-9, -8, -6, -4, -3, -2, 0, 1, 2, 3, 5, 7, 8, 9] 
-        arr2 = []
+    # def test_binary_search(self):
+    #     arr1 = [-9, -8, -6, -4, -3, -2, 0, 1, 2, 3, 5, 7, 8, 9] 
+    #     arr2 = []
 
-        self.assertEqual(binary_search(arr1, -8), 1)
-        self.assertEqual(binary_search(arr1, 0), 6)
-        self.assertEqual(binary_search(arr2, 6), -1)
-        self.assertEqual(binary_search(arr2, 0), -1)
+    #     self.assertEqual(binary_search(arr1, -8), 1)
+    #     self.assertEqual(binary_search(arr1, 0), 6)
+    #     self.assertEqual(binary_search(arr2, 6), -1)
+    #     self.assertEqual(binary_search(arr2, 0), -1)
 
     
     # def test_binary_search_recursive(self):
@@ -35,14 +35,14 @@ class SortingTest(unittest.TestCase):
     #     self.assertEqual(binary_search_recursive(arr2, 0, 0, len(arr1)-1), -1)
 
 
-    # def test_selection(self):
-    #     arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
-    #     arr2 = []
-    #     arr3 = [0, 1, 2, 3, 4, 5]
+    def test_selection(self):
+        arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+        arr2 = []
+        arr3 = [0, 1, 2, 3, 4, 5]
 
-    #     self.assertEqual(selection_sort(arr1), [0,1,2,3,4,5,6,7,8,9])
-    #     self.assertEqual(selection_sort(arr2), [])
-    #     self.assertEqual(selection_sort(arr3), [0,1,2,3,4,5])
+        self.assertEqual(selection_sort(arr1), [0,1,2,3,4,5,6,7,8,9])
+        self.assertEqual(selection_sort(arr2), [])
+        self.assertEqual(selection_sort(arr3), [0,1,2,3,4,5])
 
 
     # def test_insertion(self):
