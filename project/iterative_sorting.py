b827513239bd79d68d3a273607ed5a8d1acd0b80
stdemicheli <stdemicheli@gmail.com>
2019-01-18 00:03:34 +0100

    Implements bubble sort.

--- project/iterative_sorting.py
+++ project/iterative_sorting.py
@@ -31,6 +31,19 @@ def insertion_sort( arr ):
 
 # STRETCH: implement the Bubble Sort function below
 def bubble_sort( arr ):
+    sorting_complete = False
+    sorted_count = 0
+
+    while not sorting_complete:
+        for i in range(0, len(arr)-1):
+            cur_value = arr[i]
+            next_value = arr[i+1]
+            if cur_value > next_value:
+                arr[i], arr[i+1] = next_value, cur_value
+                sorted_count = sorted_count + 1
+
+        sorting_complete = True if sorted_count == 0 else False
+        sorted_count = 0
 
     return arr
 
@@ -38,14 +51,14 @@ def bubble_sort( arr ):
 # STRETCH: implement the Count Sort function below
 def count_sort( arr ):
     max_value = max(arr) if len(arr) > 0 else 0
-    index_list = list(range(0, max_value+1))
-    count_list = [0] * (max_value + 1)
-    result = []
+    index_list = list(range(0, max_value+1)) # Creates a list from 0 to the highest value in arr.
+    count_list = [0] * (max_value + 1) # Creates a list of zeroes that has the same length as index list.
+    result = [] # Sorted result
 
     for i in arr:
         if i < 0:
             return "Error, negative numbers not allowed in Count Sort"
-        count_list[i] += 1
+        count_list[i] += 1 # Increment count at a given index
 
     for index, i in enumerate(count_list):
         for j in range(0, i):
