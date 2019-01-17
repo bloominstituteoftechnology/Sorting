1ae55fe96b9f8f372b2201b679874195e744f3a3
Brandon Strellis <bstrellis@gmail.com>
2019-01-17 17:25:56 -0500

    Linear, binary, and binary-recursive done.

--- project/searching.py
+++ project/searching.py
@@ -1,9 +1,12 @@
 # STRETCH: implement Linear Search				
 def linear_search(arr, target):
   
-  # TO-DO: add missing code
+  for i in range(len(arr)):
+    if arr[i] == target:
+      return 1
 
-   return -1   # not found
+  return -1   # not found
+
 
 
 # STRETCH: write an iterative implementation of Binary Search 
@@ -11,20 +14,44 @@ def binary_search(arr, target):
 
   if len(arr) == 0:
     return -1 # array empty
-    
-  low = 0
-  high = len(arr)-1
 
-  # TO-DO: add missing code
+  arr.sort() # arr must be sorted for Binary Search
+    
+  while len(arr) > 0:
+    pivot_index = int( len(arr) / 2 )
+    pivot = arr[pivot_index]
+
+    if pivot == target:
+      return 1
+    
+    elif pivot > target:
+      arr = arr[:pivot_index]
+    
+    else:
+      arr = arr[pivot_index + 1:]
 
   return -1 # not found
 
 
-# STRETCH: write a recursive implementation of Binary Search 
-def binary_search_recursive(arr, target, low, high):
-  
-  middle = (low+high)/2
 
+# STRETCH: write a recursive implementation of Binary Search 
+def binary_search_recursive(arr, target):
+  
   if len(arr) == 0:
     return -1 # array empty
-  # TO-DO: add missing if/else statements, recursive calls
+  
+  arr.sort() # in production I would sort the list before passing to the recursive function so that I didn't sort a sorted list, but, hey, this is a first pass solution
+
+  pivot_index = int( len(arr) / 2 )
+  pivot = arr[pivot_index]
+
+  if pivot == target:
+    return 1
+
+  elif pivot < target:
+    return binary_search_recursive(arr[pivot_index + 1:], target)
+  
+  else:
+    return binary_search_recursive(arr[:pivot_index], target)
+
+print(binary_search_recursive([1, -1, 5, 6, 8, 2, 4, 5], 1))
