53e539c7c361bae0b04e653cf53a740c48f7fccd
alejandrok93 <alejandrok93@gmail.com>
2019-01-17 14:21:36 -0800

    Quick sort, done

--- project/recursive_sorting.py
+++ project/recursive_sorting.py
@@ -44,9 +44,24 @@ def merge_sort_in_place(arr, l, r):
 
 # TO-DO: implement the Quick Sort function below
 def quick_sort( arr, low, high ):
+    
+    if len(arr) < 2:
+        return arr
 
+    pivot = arr[0]
+    left = []
+    right = []
+    for i in range(1, len(arr)):
+        if arr[i] < pivot:
+            left.append(arr[i])
+        elif arr[i] > pivot:
+            right.append(arr[i])
+
+    arr = [*quick_sort(left, 0, len(left)-1), pivot, *quick_sort(right, 0, len(right)-1)]
+    
     return arr
 
+print(quick_sort([4,1,3,2,5, 8, 6, 9, 7], 0, 4))
 
 # STRETCH: implement the Timsort function below
 # hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
---
+++ project/test.py
@@ -0,0 +1,9 @@
+def recur(n):
+  if n <= 0:
+    
+    return 1
+  print('hello')
+  return recur(n-1)
+
+
+recur(5)
