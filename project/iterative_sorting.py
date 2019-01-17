b43549df3a499c311794c8756124fef67e738c82
Ryan Walker <rytwalker@gmail.com>
2019-01-17 17:54:21 -0500

    fixed counting sort to work better

--- project/iterative_sorting.py
+++ project/iterative_sorting.py
@@ -59,7 +59,7 @@ def count_sort(arr, maximum=-1):
         if i > maximum:
             maximum = i
     # create an array of k zeros
-    count = [0] * (maximum + 1)
+    count = [0] * maximum
     output = [0] * len(arr)
     # count number of each arr[i]
     for i in arr:
@@ -68,6 +68,7 @@ def count_sort(arr, maximum=-1):
         count[i] += count[i - 1]
     for i in range(len(arr)):
         output[count[arr[i]]-1] = arr[i]
+        count[arr[i]] -= 1
 
     return output
 
