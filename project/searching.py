33847157b890738e3e117340b50318938ee78cc8
sumi419 <sasgar04@gmail.com>
2019-01-16 19:03:38 -0800

    linear search

--- project/searching.py
+++ project/searching.py
@@ -2,10 +2,16 @@
 def linear_search(arr, target):
   
   # TO-DO: add missing code
-
-   return -1   # not found
-
-
+  for element in arr:
+    if element == target:
+      return element 
+  return -1   # not found
+  
+  # for i in range (0, len(arr)): 
+  #   if (arr[i] == target): 
+  #     return arr[i]; # returns the value if found
+  # return -1
+print(f'This is linear search {linear_search([1, 4, 6, 8], 6)}')
 # STRETCH: write an iterative implementation of Binary Search 
 def binary_search(arr, target):
 
