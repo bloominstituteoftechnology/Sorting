30b9c93658464abbd7d53ea683dea94ad322e308
michaelagard <mdagard@gmail.com>
2019-01-16 19:16:38 -0500

    first try

--- project/iterative_sorting.py
+++ project/iterative_sorting.py
@@ -19,10 +19,21 @@ def selection_sort(arr):
 
 
 def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        # cur_index = i
+        while arr[i] < arr[i - 1]:
+            temp = arr[i - 1]
+            arr[i - 1] = arr[i]
+            arr[i] = temp
+            # cur_index -= 1
     return arr
 
 
+print(insertion_sort([22, 55, 77, 44, 66, 88, 11, 33]))
+
 # STRETCH: implement the Bubble Sort function below
+
+
 def bubble_sort(arr):
 
     return arr
