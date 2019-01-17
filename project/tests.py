427b680394a3d8a1286597c9673ee4b6be24826c
Conner Alegre <cga0@pm.me>
2019-01-16 17:06:42 -0700

    Complete binary search recursive version

--- project/tests.py
+++ project/tests.py
@@ -25,14 +25,14 @@ class SortingTest(unittest.TestCase):
         self.assertEqual(binary_search(arr2, 0), -1)
 
     
-    # def test_binary_search_recursive(self):
-    #     arr1 = [-9, -8, -6, -4, -3, -2, 0, 1, 2, 3, 5, 7, 8, 9] 
-    #     arr2 = []
+    def test_binary_search_recursive(self):
+        arr1 = [-9, -8, -6, -4, -3, -2, 0, 1, 2, 3, 5, 7, 8, 9]
+        arr2 = []
 
-    #     self.assertEqual(binary_search_recursive(arr1, -8, 0, len(arr1)-1), 1)
-    #     self.assertEqual(binary_search_recursive(arr1, 0, 0, len(arr1)-1), 6)
-    #     self.assertEqual(binary_search_recursive(arr2, 6, 0, len(arr1)-1), -1)
-    #     self.assertEqual(binary_search_recursive(arr2, 0, 0, len(arr1)-1), -1)
+        self.assertEqual(binary_search_recursive(arr1, -8, 0, len(arr1)-1), 1)
+        self.assertEqual(binary_search_recursive(arr1, 0, 0, len(arr1)-1), 6)
+        self.assertEqual(binary_search_recursive(arr2, 6, 0, len(arr2)-1), -1)
+        self.assertEqual(binary_search_recursive(arr2, 0, 0, len(arr2)-1), -1)
 
 
     def test_selection(self):
