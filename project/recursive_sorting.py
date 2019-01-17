260303c563520df4293f3be853855844caf251ee
Troy Amlee <troyamlee19@gmail.com>
2019-01-17 17:46:32 -0600

    [stretch] implemented tim sort

--- project/recursive_sorting.py
+++ project/recursive_sorting.py
@@ -85,7 +85,7 @@ def partition(arr, low, high):
     # arr[high] = temp
     arr[i + 1], arr[high] = arr[high], arr[i + 1]
     return i + 1
-    
+
 def quick_sort( arr, low, high ):
     
     if low < high:
@@ -104,6 +104,62 @@ print("Quick_Sort With Different Array: ", arr)
 # print(quick_sort(an_array, 0, len(an_array) - 1))
 # STRETCH: implement the Timsort function below
 # hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
-def timsort( arr ):
+RUN = 32
+def insertion_sort(arr, left, right):
+    for i in range(left + 1, right + 1):
+        temp = arr[i]
+        j = i - 1
+        while arr[j] > temp and j >= left:
 
+            arr[j+1] = arr[j]
+            j -= 1
+
+        arr[j + 1] = temp
+
+def merge_tim(arr, l, m, r):
+
+    len1, len2 = m - l + 1, r - m
+    left, right = [], []
+    for i in range(0, len1):
+        left.append(arr[l + i])
+    for i in range(0, len2):
+        right.append(arr[m + 1 + i])
+
+    i, j, k = 0, 0, l
+
+    while i < len1 and j < len2:
+
+        if left[i] <= right[j]:
+            arr[k] = left[i]
+            i += 1
+        else:
+            arr[k] = right[j]
+            j += 1
+        k += 1
+
+    while i < len1:
+
+        arr[k] = left[i]
+        k += 1
+        i += 1
+
+    while j < len2:
+        arr[k] = right[j]
+        k += 1
+        j += 1
+
+
+def timsort( arr, n ):
+    for i in range(0, n, RUN):
+        insertion_sort(arr, i, min((i+31), (n - 1)))
+
+    size = RUN
+    while size < n:
+        for left in range(0, n, 2*size):
+            mid = left + size - 1
+            right = min((left + 2*size - 1), (n - 1))
+            merge_tim(arr, left, mid, right)
+        size = 2*size
     return arr
+
+print("Timmah!: ", timsort(an_array, len(an_array)))
