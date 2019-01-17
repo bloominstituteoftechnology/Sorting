50b54e3f7b8181d42ac5febf97466f2926992e51
codejoncode <jonathanjholloway@gmail.com>
2019-01-16 18:55:48 -0600

    all of the required sorting algos from iterative_sorting.py is completed

--- project/iterative_sorting.py
+++ project/iterative_sorting.py
@@ -6,11 +6,15 @@ def selection_sort( arr ):
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-             
+        for j in range(i, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
 
 
 
         # TO-DO: swap
+        arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]
+
 
 
 
@@ -20,12 +24,33 @@ def selection_sort( arr ):
 
 # TO-DO: implement the Insertion Sort function below
 def insertion_sort( arr ):
-
+    i  = 1
+    while i < len(arr):
+        x = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > x:
+            arr[j+1] = arr[j]
+            j = j - 1
+        #end of nested while 
+        arr[j+1] = x
+        i = i + 1 
+    #end of parent while loop 
     return arr
 
 
 # STRETCH: implement the Bubble Sort function below
 def bubble_sort( arr ):
+    """
+    O(n^2) time complexity
+    should be used with smaller lists 
+    can be used with semi sorted lists. 
+    arr must be an iterable perferably an array type. 
+    Will sort in ascending order. 
+    """
+    for element in arr:
+        for index in range (len(arr) - 1):
+        if arr[index] > arr[index + 1]:
+            arr[index], arr[index+1] = arr[index+1], arr[index]
 
     return arr
 
