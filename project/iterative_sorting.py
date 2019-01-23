9b8d128ccec89b5bd9600846a340aee6f23085ba
unknown <benvavidez_david13@yahoo.com>
2019-01-23 14:06:43 -0800

    redid selective sorting

--- project/iterative_sorting.py
+++ project/iterative_sorting.py
@@ -2,22 +2,26 @@
 def selection_sort( arr ):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
+        min_index=i 
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-        for j in range(cur_index, len(arr)):
-            if arr[j] < arr[smallest_index]:
-                smallest_index = j
+        for j in range(i+1, len(arr)):
+            if arr[j] < arr[min_index]:
+                min_index = j
         # TO-DO: swap
-        temp = arr[smallest_index]
-        arr[smallest_index] = arr[cur_index]
-        arr[cur_index] = temp
+        temp = arr[min_index]
+        arr[min_index] = arr[i]
+        arr[i] = temp
     return arr
+
+print(selection_sort(arr = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]))
+
 
 
 # TO-DO: implement the Insertion Sort function below
 def insertion_sort( arr ):
+
+
 
 
     return arr
