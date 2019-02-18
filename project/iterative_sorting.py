9d4146219218ba9ffe51146e30db939246bc958e
Axolotl <samuelmachat@gmail.com>
2019-02-18 16:51:26 -0500

    selection sort done

--- project/iterative_sorting.py
+++ project/iterative_sorting.py
@@ -1,36 +1,36 @@
 # Complete the selection_sort() function below in class with your instructor
-def selection_sort( arr ):
+def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
         # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
-
-
+        # (hint, can do in 3 loc)
+        for j in range(cur_index, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
 
         # TO-DO: swap
-
-
-
+        temp = arr[smallest_index]
+        arr[smallest_index] = arr[cur_index]
+        arr[cur_index] = temp
 
     return arr
 
 
 # TO-DO: implement the Insertion Sort function below
-def insertion_sort( arr ):
+def insertion_sort(arr):
 
     return arr
 
 
 # STRETCH: implement the Bubble Sort function below
-def bubble_sort( arr ):
+def bubble_sort(arr):
 
     return arr
 
 
 # STRETCH: implement the Count Sort function below
-def count_sort( arr, maximum=-1 ):
+def count_sort(arr, maximum=-1):
 
     return arr
--- project/tests.py
+++ project/tests.py
@@ -1,7 +1,8 @@
 import unittest
 from searching import *
-from iterative_sorting import * 
+from iterative_sorting import *
 from recursive_sorting import merge_sort, merge_sort_in_place, quick_sort
+
 
 class SortingTest(unittest.TestCase):
 
@@ -14,9 +15,8 @@ class SortingTest(unittest.TestCase):
     #     self.assertEqual(linear_search(arr1, 0), 6)
     #     self.assertEqual(linear_search(arr2, 3), -1)
 
-
     # def test_binary_search(self):
-    #     arr1 = [-9, -8, -6, -4, -3, -2, 0, 1, 2, 3, 5, 7, 8, 9] 
+    #     arr1 = [-9, -8, -6, -4, -3, -2, 0, 1, 2, 3, 5, 7, 8, 9]
     #     arr2 = []
 
     #     self.assertEqual(binary_search(arr1, -8), 1)
@@ -24,9 +24,8 @@ class SortingTest(unittest.TestCase):
     #     self.assertEqual(binary_search(arr2, 6), -1)
     #     self.assertEqual(binary_search(arr2, 0), -1)
 
-    
     # def test_binary_search_recursive(self):
-    #     arr1 = [-9, -8, -6, -4, -3, -2, 0, 1, 2, 3, 5, 7, 8, 9] 
+    #     arr1 = [-9, -8, -6, -4, -3, -2, 0, 1, 2, 3, 5, 7, 8, 9]
     #     arr2 = []
 
     #     self.assertEqual(binary_search_recursive(arr1, -8, 0, len(arr1)-1), 1)
@@ -34,26 +33,23 @@ class SortingTest(unittest.TestCase):
     #     self.assertEqual(binary_search_recursive(arr2, 6, 0, len(arr1)-1), -1)
     #     self.assertEqual(binary_search_recursive(arr2, 0, 0, len(arr1)-1), -1)
 
-
     def test_selection(self):
         arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
         arr2 = []
         arr3 = [0, 1, 2, 3, 4, 5]
 
-        self.assertEqual(selection_sort(arr1), [0,1,2,3,4,5,6,7,8,9])
+        self.assertEqual(selection_sort(arr1), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
         self.assertEqual(selection_sort(arr2), [])
-        self.assertEqual(selection_sort(arr3), [0,1,2,3,4,5])
+        self.assertEqual(selection_sort(arr3), [0, 1, 2, 3, 4, 5])
 
+    # def test_insertion(self):
+    #     arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+    #     arr2 = []
+    #     arr3 = [0, 1, 2, 3, 4, 5]
 
-    def test_insertion(self):
-        arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
-        arr2 = []
-        arr3 = [0, 1, 2, 3, 4, 5]
-
-        self.assertEqual(insertion_sort(arr1), [0,1,2,3,4,5,6,7,8,9])
-        self.assertEqual(insertion_sort(arr2), [])
-        self.assertEqual(insertion_sort(arr3), [0,1,2,3,4,5])
-
+    #     self.assertEqual(insertion_sort(arr1), [0,1,2,3,4,5,6,7,8,9])
+    #     self.assertEqual(insertion_sort(arr2), [])
+    #     self.assertEqual(insertion_sort(arr3), [0,1,2,3,4,5])
 
     # def test_bubble(self):
     #     arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
@@ -64,7 +60,6 @@ class SortingTest(unittest.TestCase):
     #     self.assertEqual(bubble_sort(arr2), [])
     #     self.assertEqual(bubble_sort(arr3), [0,1,2,3,4,5])
 
-
     # def test_count(self):
     #     arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
     #     arr2 = []
@@ -74,18 +69,16 @@ class SortingTest(unittest.TestCase):
     #     self.assertEqual(count_sort(arr2), [])
     #     self.assertEqual(count_sort(arr3), "Error, negative numbers not allowed in Count Sort")
 
+    # def test_merge(self):
+    #     arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+    #     arr2 = []
+    #     arr3 = [2]
+    #     arr4 = [0, 1, 2, 3, 4, 5]
 
-    def test_merge(self):
-        arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
-        arr2 = []
-        arr3 = [2]
-        arr4 = [0, 1, 2, 3, 4, 5]
-
-        self.assertEqual(merge_sort(arr1), [0,1,2,3,4,5,6,7,8,9])
-        self.assertEqual(merge_sort(arr2), [])
-        self.assertEqual(merge_sort(arr3), [2])
-        self.assertEqual(merge_sort(arr4), [0,1,2,3,4,5])
-
+    #     self.assertEqual(merge_sort(arr1), [0,1,2,3,4,5,6,7,8,9])
+    #     self.assertEqual(merge_sort(arr2), [])
+    #     self.assertEqual(merge_sort(arr3), [2])
+    #     self.assertEqual(merge_sort(arr4), [0,1,2,3,4,5])
 
     # def test_merge_in_place(self):
     #     arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
@@ -98,15 +91,14 @@ class SortingTest(unittest.TestCase):
     #     self.assertEqual(merge_sort_in_place(arr3, 0, len(arr3)-1), [2])
     #     self.assertEqual(merge_sort_in_place(arr4, 0, len(arr4)-1), [0,1,2,3,4,5])
 
+    # def test_quick(self):
+    #     arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+    #     arr2 = []
+    #     arr3 = [0, 1, 2, 3, 4, 5]
 
-    def test_quick(self):
-        arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
-        arr2 = []
-        arr3 = [0, 1, 2, 3, 4, 5] 
-
-        self.assertEqual(quick_sort(arr1, 0, len(arr1)-1), [0,1,2,3,4,5,6,7,8,9])
-        self.assertEqual(quick_sort(arr2, 0, len(arr2)-1), [])
-        self.assertEqual(quick_sort(arr3, 0, len(arr3)-1), [0,1,2,3,4,5])
+    #     self.assertEqual(quick_sort(arr1, 0, len(arr1)-1), [0,1,2,3,4,5,6,7,8,9])
+    #     self.assertEqual(quick_sort(arr2, 0, len(arr2)-1), [])
+    #     self.assertEqual(quick_sort(arr3, 0, len(arr3)-1), [0,1,2,3,4,5])
 
 
 if __name__ == '__main__':
