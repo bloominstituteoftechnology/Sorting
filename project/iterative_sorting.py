8b039542db887a24a65052bbde91b47163e80e3f
Axolotl <samuelmachat@gmail.com>
2019-02-18 17:25:43 -0500

    complete insertion sort

--- project/iterative_sorting.py
+++ project/iterative_sorting.py
@@ -20,6 +20,13 @@ def selection_sort(arr):
 
 # TO-DO: implement the Insertion Sort function below
 def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        temp = arr[i]
+        j = i-1
+        while(j >= 0 and temp < arr[j]):
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = temp
 
     return arr
 
