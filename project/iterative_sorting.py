274ea8c0d13cd6ee5d798656b36ecf91ac988156
Carlos Lantigua <clantigua954@gmail.com>
2019-02-19 09:57:50 -0500

    messing with different ways to write sorts

--- project/iterative_sorting.py
+++ project/iterative_sorting.py
@@ -1,4 +1,8 @@
+array = [54, 26, 93, 17, 77, 31, 44, 55, 20, 55,
+         46, 99, 35, 62, 1, 14, 67, 59, 49, 29, 11]
 # Complete the selection_sort() function below in class with your instructor
+
+
 def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
@@ -26,35 +30,61 @@ def selection_sort(arr):
 
 def insertion_sort(arr):
     # separate the first element from the rest of the array, think about it as a sorted list of one element
+    # for i in range(1, len(arr)):
+    #     current = arr[i]
+    #     pos = i
+    #     while (pos > 0 and arr[pos - 1] > current):
+    #         arr[pos] = arr[pos - 1]
+    #         pos = pos - 1
+    #     arr[pos] = current
+    # return arr
     for i in range(1, len(arr)):
-        current = arr[i]
-        pos = i
-        while (pos > 0 and arr[pos - 1] > current):
-            arr[pos] = arr[pos - 1]
-            pos = pos - 1
-        arr[pos] = current
+        j = i - 1
+        while arr[j] > arr[j+1] and j >= 0:
+            arr[j], arr[j+1] = arr[j+1], arr[j]
+            j -= 1
     return arr
 
 
-# print(insertion_sort([54, 26, 93, 17, 77, 31, 44, 55, 20]))
+def insertion_sort_noSwap(arr):
+    for i in range(1, len(arr)):
+        curNum = arr[i]  # save teh current number
+        for j in range(i - 1, 0, -1):
+            if arr[j] > curNum:
+                arr[j + 1] = arr[j]
+            else:
+                arr[j + 1] = curNum
+                break
+    return arr
 
+
+print(insertion_sort(array))
+print(insertion_sort_noSwap(array))
 # STRETCH: implement the Bubble Sort function below
 
 # Walk through the array, comparing each element to its right neighbor. If it's smaller than that neighbor, swap the elements. Repeat this until you make it through an entire pass without any swaps.
+
+
 def bubble_sort(arr):
     for i in range(len(arr)-1, 0, -1):
         for j in range(i):
-            if (arr[j] > arr[j + 1]):
-                temp = arr[j]
-                arr[j] = arr[j + 1]
-                arr[j + 1] = temp
+            first = arr[j]
+            second = arr[j + 1]
+            if (first > second):
+                first, second = second, first
+                # temp = first
+                # first = second
+                # second = temp
     return arr
 
 
-print(bubble_sort([54, 26, 93, 17, 77, 31, 44, 55, 20]))
+# print(bubble_sort([54, 26, 93, 17, 77, 31, 44, 55, 20]))
 # STRETCH: implement the Count Sort function below
 
 
 def count_sort(arr, maximum=-1):
 
     return arr
+
+
+bubble_sort(array)
--- project/recursive_sorting.py
+++ project/recursive_sorting.py
@@ -1,11 +1,14 @@
-### helper function
-def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
+array = [54, 26, 93, 17, 77, 31, 44, 55, 20, 55, 46, 99, 35, 62, 1, 14, 67, 59, 49, 29, 11]
+# ### helper function
+
+
+def merge(arrA, arrB):
+    elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
     a = 0
     b = 0
     # since arrA and arrB already sorted, we only need to compare the first element of each when merging!
-    for i in range( 0, elements ):
+    for i in range(0, elements):
         if a >= len(arrA):    # all elements in arrA have been merged
             merged_arr[i] = arrB[b]
             b += 1
@@ -20,36 +23,42 @@ def merge( arrA, arrB ):
             b += 1
     return merged_arr
 
+# recursive sorting function
 
-### recursive sorting function
-def merge_sort( arr ):
-    if len( arr ) > 1:
-        left = merge_sort( arr[ 0 : len( arr ) / 2 ] )
-        right = merge_sort( arr[ len( arr ) / 2 : ] )
-        arr = merge( left, right )   # merge() defined later
+
+def merge_sort(arr):
+    if len(arr) > 1:
+        left = merge_sort(arr[0: len(arr) / 2])
+        right = merge_sort(arr[len(arr) / 2:])
+        arr = merge(left, right)   # merge() defined later
     return arr
 
 
+merge_sort(array)
+
 # STRETCH: implement an in-place merge sort algorithm
+
+
 def merge_in_place(arr, start, mid, end):
     # TO-DO
 
     return arr
 
-def merge_sort_in_place(arr, l, r): 
+
+def merge_sort_in_place(arr, l, r):
     # TO-DO
 
     return arr
 
 
 # TO-DO: implement the Quick Sort function below USING RECURSION
-def quick_sort( arr, low, high ):
+def quick_sort(arr, low, high):
 
     return arr
 
 
 # STRETCH: implement the Timsort function below
 # hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
-def timsort( arr ):
+def timsort(arr):
 
     return arr
