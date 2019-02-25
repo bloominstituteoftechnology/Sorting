460ab6fec7cde21fe2f91f4821df0dd0fca09da4
Chad Jemmett <chad.jemmett@gmail.com>
2019-02-25 16:46:20 -0700

    STILL working in insertion sort

--- project/iterative_sorting.py
+++ project/iterative_sorting.py
@@ -19,16 +19,24 @@ def selection_sort( arr ):
 # TO-DO: implement the Insertion Sort function below
 def insertion_sort(arr):
     # We set the first item in the array as sorted.
+            # We check the temp agains the neighbor to it's left.
+            # if temp is larger. Leave it there.
+            # # While the temp variable is larger than it's neighbor we leave it there. then we go to the next item in the array.
+            # We set that to temp.
+            # Then we check on the way down.
+            # Checking each one against the left neighbor. When we find the spot where there the value is less than. We insert the temp item there.
+            # We keep doing this. as the for loop increments. And when it does get all the way to the end. jk
+
     # for item in range(1, len(arr)):
-    start the for loop.
-    first sorted.
-    the second is the temp
-    Start the while loop.
-        while the temp is larger than the items down  to 0
-            we compare the temp item to it's neighbor to the left
-            if the temp variable is larger. Then we stop. If it is less than, 
-                We keep moving down until there is one that is less than. Or rather the temp variable is larger. than the one on the left.
-                If the variable on the left is smaller, then we break the loop. Set a new temp and start over.
+    # start the for loop.
+    # first sorted.
+    # the second is the temp
+    # Start the while loop.
+    #     while the temp is larger than the items down  to 0
+    #         we compare the temp item to it's neighbor to the left
+    #         if the temp variable is larger. Then we stop. If it is less than, 
+    #             We keep moving down until there is one that is less than. Or rather the temp variable is larger. than the one on the left.
+    #             If the variable on the left is smaller, then we break the loop. Set a new temp and start over.
 
 
 
