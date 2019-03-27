5a1eff85b3508cc2e8cd89021c86f21b6f20c5eb
Sean Chen <skypemaster007@gmail.com>
2019-03-27 11:21:01 -0700

    Comment out stretch goal tests

--- src/recursive_sorting/test_recursive.py
+++ src/recursive_sorting/test_recursive.py
@@ -16,16 +16,16 @@ class RecursiveSortingTests(unittest.TestCase):
         self.assertEqual(merge_sort(arr4), [0,1,2,3,4,5])
         self.assertEqual(merge_sort(arr5), arr5.sort())
 
+    # Uncomment this test to test your in-place merge sort implementation
+    # def test_in_place_merge_sort(self):
+    #     arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+    #     arr2 = []
+    #     arr3 = [2]
+    #     arr4 = [0, 1, 2, 3, 4, 5]
+    #     arr5 = random.sample(range(200), 50)
 
-    def test_in_place_merge_sort(self):
-        arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
-        arr2 = []
-        arr3 = [2]
-        arr4 = [0, 1, 2, 3, 4, 5]
-        arr5 = random.sample(range(200), 50)
-
-        self.assertEqual(merge_sort_in_place(arr1, 0, len(arr1)-1), [0,1,2,3,4,5,6,7,8,9])
-        self.assertEqual(merge_sort_in_place(arr2, 0, len(arr2)-1), [])
-        self.assertEqual(merge_sort_in_place(arr3, 0, len(arr3)-1), [2])
-        self.assertEqual(merge_sort_in_place(arr4, 0, len(arr4)-1), [0,1,2,3,4,5])
-        self.assertEqual(merge_sort_in_place(arr5, 0, len(arr5)-1), arr5.sort())
+    #     self.assertEqual(merge_sort_in_place(arr1, 0, len(arr1)-1), [0,1,2,3,4,5,6,7,8,9])
+    #     self.assertEqual(merge_sort_in_place(arr2, 0, len(arr2)-1), [])
+    #     self.assertEqual(merge_sort_in_place(arr3, 0, len(arr3)-1), [2])
+    #     self.assertEqual(merge_sort_in_place(arr4, 0, len(arr4)-1), [0,1,2,3,4,5])
+    #     self.assertEqual(merge_sort_in_place(arr5, 0, len(arr5)-1), arr5.sort())
