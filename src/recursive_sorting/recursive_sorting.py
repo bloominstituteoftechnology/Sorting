37fa5de43f1204098d5dceb6b518426a491c13b2
Nikita Thomas <nikcxz@gmail.com>
2019-03-27 18:20:10 -0400

    merge

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,8 +1,28 @@
-# TO-DO: complete the helpe function below to merge 2 sorted arrays
+# TO-DO: complete the helper function below to merge 2 sorted arrays
 def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
-    merged_arr = [0] * elements
+
     # TO-DO
+    a = 0
+    b = 0
+    minLen = min(len(arrA), len(arrB))
+    merged_arr = [0] * (minLen*2 - 1)
+
+    longerArr: list
+    if len(arrA) > len(arrB):
+        longerArr = arrA
+    else:
+        longerArr = arrB
+
+    print(minLen)
+    for i in range(0, (minLen*2)-1):
+        if arrA[a] <= arrB[b]:
+            merged_arr[i] = arrA[a]
+            a += 1
+        else:
+            merged_arr[i] = arrB[b]
+            b += 1
+    merged_arr += longerArr[(minLen)-1:]
     
     return merged_arr
 
@@ -31,3 +51,9 @@ def merge_sort_in_place(arr, l, r):
 def timsort( arr ):
 
     return arr
+arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+arr2 = []
+arr3 = [2]
+arr4 = [0, 1, 2, 3, 4, 5]
+
+print(merge(arr1, arr4))
