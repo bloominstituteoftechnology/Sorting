63e8f35a6808dd766a6c68fcd0da3642ce206fb7
Sean Chen <skypemaster007@gmail.com>
2019-03-27 11:10:03 -0700

    Break each section into their own directories with their own READMEs

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -23,7 +23,7 @@ def binary_search(arr, target):
 # STRETCH: write a recursive implementation of Binary Search 
 def binary_search_recursive(arr, target, low, high):
   
-  middle = (low+high)/2
+  middle = (low+high)//2
 
   if len(arr) == 0:
     return -1 # array empty
