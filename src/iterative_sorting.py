c4951a23b4d23b187fe7da349ba8586d298fc268
CarloC24 <carlo.clamucha@gmail.com>
2019-03-27 18:41:43 -0500

    added intertive search

--- src/iterative_sorting.py
+++ src/iterative_sorting.py
@@ -1,30 +1,35 @@
-# TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
+# TO-DO: Complete the selection_sort() function below
+def selection_sort(arr):
     # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
-
-
-
-        # TO-DO: swap
-
-
-
+    for i in range(0, len(arr)):
+        current_index = i
+        min_value_index = current_index
+        for j in range(current_index, len(arr) - 1):
+            if arr[min_value_index] > arr[j]:
+                min_value_index = j
+        temp = arr[min_value_index]
+        arr[min_value_index] = arr[current_index]
+        arr[current_index] = temp
 
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
-def bubble_sort( arr ):
+def bubble_sort(arr):
+    swapping_occured = True
+    while swapping_occured:
+        swapping_occured = False
+    for i in range(0, len(arr) - 1):
+        if arr[i] > arr[i + 1]:
+            swapping_occured = True
+            temp = arr[i]
+            arr[i] = arr[i + 1]
+            arr[i + 1] = temp
 
     return arr
 
 
 # STRETCH: implement the Count Sort function below
-def count_sort( arr, maximum=-1 ):
+def count_sort(arr, maximum=-1):
 
     return arr
