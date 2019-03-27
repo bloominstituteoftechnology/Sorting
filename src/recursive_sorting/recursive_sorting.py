357a5c1cb675483a29aaeb9737de2a74fe6bcd61
Nikita Thomas <nikcxz@gmail.com>
2019-03-27 18:37:46 -0400

    half of merge done

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -14,8 +14,10 @@ def merge( arrA, arrB ):
     else:
         longerArr = arrB
 
-    print(minLen)
     for i in range(0, (minLen*2)-1):
+        print(f"arra[a] = {arrA[a]}")
+        print(f"arrb[b] = {arrB[b]}")
+        print(f'count = {i}')
         if arrA[a] <= arrB[b]:
             merged_arr[i] = arrA[a]
             a += 1
@@ -29,9 +31,14 @@ def merge( arrA, arrB ):
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
-    # TO-DO
+    if len(arr) == 0 or len(arr) == 1:
+        return arr
+    else:
+        middle = len(arr)//2
+        left = merge_sort(arr[:middle])
+        right = merge_sort(arr[middle:])
 
-    return arr
+        return merge(left,right)
 
 
 # STRETCH: implement an in-place merge sort algorithm
@@ -55,5 +62,10 @@ arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 arr2 = []
 arr3 = [2]
 arr4 = [0, 1, 2, 3, 4, 5]
+arr5 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
 
-print(merge(arr1, arr4))
+#print(merge(arr1, arr4))
+#print(merge([0], [1]))
+
+#print(merge_sort([0,1]))
+print(merge_sort(arr5))
