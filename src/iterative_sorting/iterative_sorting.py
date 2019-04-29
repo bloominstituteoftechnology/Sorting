89090a3ef8e916dfbf08519e57fe54cc6f648a12
Dillon McElhinney <dillon.mce@gmail.com>
2019-04-29 14:33:54 -0600

    Finish count sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -40,5 +40,33 @@ def bubble_sort( arr ):
 
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
+    # Validate Inputs
+    if len(arr) < 2:
+        return arr
 
-    return arr
+    if maximum == -1:
+        maximum = max(arr)
+
+    # Set up variables    
+    count = [0 for x in range(maximum+1)]
+    result = [0 for x in range(len(arr))]
+
+    # Count elements
+    for num in arr:
+        if num < 0:
+            return "Error, negative numbers not allowed in Count Sort"
+        count[num] += 1
+    
+    for i in range(1, len(count)):
+        count[i] += count[i-1]
+
+    # Set each element at the right place
+    for num in arr:
+        result[count[num]-1] = num
+        count[num] -= 1
+
+    return result
+
+'''Bogo sort is an incredibly inefficient algorithm that basically just
+shuffles all of the elements and then checks to see if they are in order.
+The complexity is O((n+1)!)'''
