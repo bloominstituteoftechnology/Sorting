5a793000c68fe293bea1bd5326d08c24a65a8dc0
Dillon McElhinney <dillon.mce@gmail.com>
2019-04-29 15:16:10 -0600

    Finish searching

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -2,8 +2,11 @@
 def linear_search(arr, target):
   
   # TO-DO: add missing code
+  for (index, item) in enumerate(arr):
+    if item == target:
+      return index
 
-   return -1   # not found
+  return -1   # not found
 
 
 # STRETCH: write an iterative implementation of Binary Search 
@@ -16,15 +19,31 @@ def binary_search(arr, target):
   high = len(arr)-1
 
   # TO-DO: add missing code
+  while low <= high:
+    mid = (low + high) // 2
+    guess = arr[mid]
+
+    if guess == target:
+      return mid
+    if guess > target:
+      high = mid - 1
+    else:
+      low = mid + 1
 
   return -1 # not found
 
 
 # STRETCH: write a recursive implementation of Binary Search 
 def binary_search_recursive(arr, target, low, high):
-  
-  middle = (low+high)//2
+  middle = (low+high) // 2 + 1
 
   if len(arr) == 0:
     return -1 # array empty
   # TO-DO: add missing if/else statements, recursive calls
+  guess = arr[middle]
+  if guess == target:
+    return middle
+  if guess > target:
+    return binary_search_recursive(arr, target, low, middle - 1)
+  else:
+    return binary_search_recursive(arr, target, middle + 1, high)
