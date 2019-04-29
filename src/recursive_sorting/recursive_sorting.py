ddf3d62fd1cce1f69f02f916e053a6cdf9d4f13f
johnoro <officialjohnorourke@gmail.com>
2019-04-29 18:53:04 -0400

    Implements quick sort

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,6 +1,25 @@
+from random import randint
+def quick_sort(arr):
+  pivot = randint(0, len(arr) - 1)
+  left = []
+  right = []
+  for num in arr[:pivot] + arr[pivot+1:]:
+    if num < arr[pivot]:
+      left.append(num)
+    else:
+      right.append(num)
+  if len(left) > 1:
+    left = quick_sort(left)
+  if len(right) > 1:
+    right = quick_sort(right)
+  return left + [arr[pivot]] + right
+
+
+print(quick_sort([1, 3, 6, 2, 13, 41, 7]))
+
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
-def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
+def merge(arrA, arrB):
+    elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
     # TO-DO
     
@@ -8,7 +27,7 @@ def merge( arrA, arrB ):
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
-def merge_sort( arr ):
+def merge_sort(arr):
     # TO-DO
 
     return arr
@@ -28,6 +47,6 @@ def merge_sort_in_place(arr, l, r):
 
 # STRETCH: implement the Timsort function below
 # hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
-def timsort( arr ):
+def timsort(arr):
 
     return arr
