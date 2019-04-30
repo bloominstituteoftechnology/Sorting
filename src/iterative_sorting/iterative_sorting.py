dec7c7daa997a268c65862ef58e28d507af3f14c
leianne louis <leilouis112@gmail.com>
2019-04-30 15:25:56 -0400

    polisheed selectionsort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,17 +1,15 @@
 # TO-DO: Complete the selection_sort() function below 
 def selection_sort( arr ):
     # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
+    for i in range(0, len(arr)):
         min_index = i
         for j in range(i+1, len(arr)):
-            if arr[j] < arr[i]:
+            if arr[min_index] > arr[j]:
                 min_index = j
-            if min_index != i:
-                arr[i], arr[min_index] = arr[min_index], arr[i]
+        arr[i], arr[min_index] = arr[min_index], arr[i]
     print(arr)
     return arr
 
-selection_sort([1, 5, 8, 4, 2, 9, 6, 0, 3, 7])
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
     swapped = True
