e980eaf29a5bf0bd48a02465aa6d08c412fa4e78
audreywelch <welch.audreyl@gmail.com>
2019-05-07 19:05:31 -0500

    Finished merge function to correctly merge pre-sorted arrays

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,17 +1,71 @@
-# TO-DO: complete the helpe function below to merge 2 sorted arrays
+# Helper function merges 2 sorted arrays
 def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
-    # TO-DO
-    
+
+    # initialize indices for accessing elements in the two arrays
+    a = 0
+    b = 0
+
+    # merged_arr = arrA + arrB
+
+    # Loop through list of elements
+    for i in range(elements):
+        
+        # if all elements from arrA have been merged
+        if a >= len(arrA):
+            # put next element in arrB into merged array
+            merged_arr[i] = arrB[b]
+            b += 1
+
+        # if all elements from arrB have been merged
+        elif b >= len(arrB):
+            # put next element in arrA into merged array
+            merged_arr[i] = arrA[a]
+            b += 1
+
+        # if A[0] is smaller than B[0]
+        elif arrA[a] < arrB[b]:
+            # put the element from A into the output array
+            merged_arr[i] = arrA[a]
+
+            # increment the index
+            a += 1
+
+        # if A[0] is bigger than B[0]
+        else: # arrA[a] >= arrB[b]:
+            # put the element from B into the output array
+            merged_arr[i] = arrB[b]
+            b += 1
+
     return merged_arr
 
+a = [1, 4, 6]
+b = [2, 3, 5]
+print(merge(a, b))
+
+c = [1, 3, 2, 0, 4, 5, 9, 7, 8, 6, 10]
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
-    # TO-DO
+
+    # While data set contains more than one item, split it in half
+    if len(arr) > 1:
+
+        # Recursively call merge_sort to continue splitting the arrays in half
+        left_partition = merge_sort(arr[:len(arr) // 2])
+        right_partition = merge_sort(arr[len(arr) // 2:])
+
+        # Merge the left and right sides together into one array
+        arr = merge(left_partition, right_partition)
 
     return arr
+
+print(merge_sort(c))
+
+
+
+
 
 
 # STRETCH: implement an in-place merge sort algorithm
