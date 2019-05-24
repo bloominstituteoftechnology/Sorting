1e76beb6cba52b034627ec4e075995e3cf4c3075
cocoitali <cmburatto@gmail.com>
2019-05-23 20:51:54 -0600

    bubble_sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -18,9 +18,18 @@ def selection_sort( arr ):
 
 
 # TO-DO:  implement the Bubble Sort function below
+#1 compare first two items
+#2 if the left > right, swap (1st item is sorted, everything else is not)
+#3 next two items, repeat above step
 def bubble_sort( arr ):
-   
+    for i in range(0, len(arr) - 1): #outer for loop
+       for j in range(0, len(arr) - 1 - i): #inner for loop
+           if arr[j] > arr[j+1]: #compare item with item on right
+               arr[j], arr[j+1] = arr[j+1], arr[j] 
+                #if item on left > item on right, swap
     return arr
+
+
 
 
 # STRETCH: implement the Count Sort function below
