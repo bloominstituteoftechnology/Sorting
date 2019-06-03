44be1ff6b2a350cedd573114368e6fee8b6cb289
Michael Beck <brit228@gmail.com>
2019-06-03 19:10:24 -0400

    Finished recursive sorting

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -4,23 +4,28 @@ def selection_sort( arr ):
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
-
-
-
-        # TO-DO: swap
-
-
-
-
+        for j in range(i, len(arr)):
+            if arr[smallest_index] > arr[j]:
+                smallest_index = j
+        if smallest_index != cur_index:
+            val = arr[cur_index]
+            arr[cur_index] = arr[smallest_index]
+            arr[smallest_index] = val
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
-
+    while True:
+        cont = False
+        for i in range(0, len(arr)-1):
+            if arr[i] > arr[i+1]:
+                cont = True
+                val = arr[i]
+                arr[i] = arr[i+1]
+                arr[i+1] = val
+        if not cont:
+            break
     return arr
 
 
--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,23 +1,25 @@
-# TO-DO: complete the helpe function below to merge 2 sorted arrays
+# TO-DO: complete the helper function below to merge 2 sorted arrays
 def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
-    # TO-DO
-    
+    for i in range(0, len(arrA)):
+        merged_arr[i] = arrA[i]
+    for i in range(0, len(arrB)):
+        merged_arr[i+len(arrA)] = arrB[i]
     return merged_arr
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
-    # TO-DO
-
+    if len(arr) != 1:
+        ind = len(arr) // 2
+        merge_sort()
     return arr
 
 
 # STRETCH: implement an in-place merge sort algorithm
 def merge_in_place(arr, start, mid, end):
-    # TO-DO
-
+    
     return arr
 
 def merge_sort_in_place(arr, l, r): 
