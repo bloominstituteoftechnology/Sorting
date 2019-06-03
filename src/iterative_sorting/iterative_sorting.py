6802739afaeee3d6737d6b4c71a5e4706846c354
Shrey <ShreyasJothish>
2019-06-03 15:32:31 -0700

    Day 1 Assignment

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,30 +1,51 @@
-# TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
+# TO-DO: Complete the selection_sort() function below
+def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
         # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
-
-
+        # (hint, can do in 3 loc)
+        for j in range(i, len(arr)):
+            if arr[smallest_index] > arr[j]:
+                smallest_index = j
 
         # TO-DO: swap
-
-
-
+        temp = arr[cur_index]
+        arr[cur_index] = arr[smallest_index]
+        arr[smallest_index] = temp
 
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
-def bubble_sort( arr ):
+def bubble_sort(arr):
+    for i in range(len(arr)-1):
+        for j in range(len(arr)-1):
+            if arr[j] > arr[j+1]:
+                temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = temp
 
     return arr
 
 
+def bubble_sort2(arr):
+    swapping_occured = True
+
+    while swapping_occured:
+        swapping_occured = False
+
+        for i in range(0, len(arr) - 1):
+            if arr[i] > arr[i + 1]:
+                swapping_occured = True
+                temp = arr[i]
+                arr[i] = arr[i+1]
+                arr[i+1] = temp
+    return arr
+
+
 # STRETCH: implement the Count Sort function below
-def count_sort( arr, maximum=-1 ):
+def count_sort(arr, maximum=-1):
 
     return arr
