4d1860bb5957a0ac8355a5c5bb3a86dc0636abc4
AustinBCole <austin.cole.chileno@gmail.com>
2019-06-03 14:15:49 -0600

    Finis selection_sort, start on bubble_sort.

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,26 +1,35 @@
 # TO-DO: Complete the selection_sort() function below 
 def selection_sort( arr ):
     # loop through n-1 elements
+    # We dont iterate over the last index because the nested loop starts at i + 1, so if we were to be at the last index we would get an `index out of range` error.
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
         # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
-
-
+        # (hint, can do in 3 loc)
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
 
         # TO-DO: swap
-
-
-
-
+        
+        swapped_value = arr[cur_index]
+        arr[cur_index] = arr[smallest_index]
+        arr[smallest_index] = swapped_value
+    print(arr)
     return arr
+
+selection_sort([0,5,2,3,1])
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
-
+    for i in range(0, len(arr) - 1):
+        if arr[i] > arr[i + 1]:
+            print(True)
+            swapped_value = arr[i]
+            arr [i] = arr[i + 1]
+            arr[i + 1] = swapped_value
     return arr
 
 
--- src/iterative_sorting/test_iterative.py
+++ src/iterative_sorting/test_iterative.py
@@ -23,7 +23,7 @@ class IterativeSortingTest(unittest.TestCase):
         self.assertEqual(bubble_sort(arr1), [0,1,2,3,4,5,6,7,8,9])
         self.assertEqual(bubble_sort(arr2), [])
         self.assertEqual(bubble_sort(arr3), [0,1,2,3,4,5])
-        self.assertEqual(selection_sort(arr4), sorted(arr4))
+        self.assertEqual(bubble_sort(arr4), sorted(arr4))
 
     # Uncomment this test to test your count_sort implementation
     # def test_counting_sort(self):
