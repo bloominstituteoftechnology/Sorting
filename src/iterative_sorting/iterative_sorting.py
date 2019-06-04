a2278d6058c7525f5981fd03bb43019fe4b695c5
Carlos Gutierrez <chgutier@gmail.com>
2019-06-03 21:14:26 -0500

    Working selection sort function

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -9,26 +9,17 @@ def selection_sort(arr):
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
+        for each in range(cur_index, (len(arr))):
+            if arr[each] < arr[smallest_index]:
+                smallest_index = each
+        # temporarely saving smallest number
+        temp_small = arr[smallest_index]
+        # swapping smallest num found to current idx num
+        # and viceversa
+        arr[smallest_index] = arr[cur_index]
+        arr[cur_index] = temp_small
         
-        # set the first unsorted element as the minimum
-        # for each of the unsorted elements
-        # if element < currentMinimum
-        # set element as new minimum
-        # swap minimum with first unsorted position
-        if arr[cur_index + 1 ] < arr[cur_index]:
-            arr[smallest_index] = arr[cur_index + 1]
-
-
-
-        # TO-DO: swap
-
-
-
-
     return arr
-
-
-# TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
 
     return arr
