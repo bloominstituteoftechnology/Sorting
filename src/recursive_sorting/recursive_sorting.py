bfa4778bf6c667a6959425189eceb620d2f8a524
Mark Oliver <mark.c.oliver@gmail.com>
2019-06-04 13:29:00 -0400

    n*log(n)

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -33,7 +33,6 @@ arrA = [1,15,8,10]
 arrB = [11, 16, 21, 26]
 # print(merge(arrA, arrB))
 
-# TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     if len(arr) < 2:
         return arr
@@ -45,32 +44,34 @@ def merge_sort( arr ):
     L = len(arr)
 
     # if len(arr) > 2:
-    assert len(arr) > 2, f'logic error arr len {len(arr)}'
-    p = arr[0]
-    arr = arr[1:]
-    arrl = []
-    arrh = []
-    dc = 1
-    for i in range(0,len(arr)):
-        if arr[i] < p:
-            arrl.append(arr[i])
-        elif arr[i] > p:
-            arrh.append(arr[i])
-        else: # equal
-            dc += 1
-    p = [p] * dc
+    assert L > 2, f'logic error arr len {len(arr)}'
+    n = L // 2
+    return merge(merge_sort(arr[0:n]), merge_sort(arr[n:]))
+    # p = arr[0]
+    # arr = arr[1:]
+    # arrl = []
+    # arrh = []
+    # dc = 1
+    # for i in range(0,len(arr)):
+    #     if arr[i] < p:
+    #         arrl.append(arr[i])
+    #     elif arr[i] > p:
+    #         arrh.append(arr[i])
+    #     else: # equal
+    #         dc += 1
+    # p = [p] * dc
     # print('checklength', L, dc + len(arrl) + len(arrh))
     # print('p', p, type(p))
-    arr = merge_sort(arrl)
-    arr.extend(p)
-    # print('type arr', type(arr))
-    arr.extend(merge_sort(arrh))
-    return arr
+    # arr = merge_sort(arrl)
+    # arr.extend(p)
+    # # print('type arr', type(arr))
+    # arr.extend(merge_sort(arrh))
+    # return arr
 
-# arrB.extend(arrA)
-# arr = arrB
-# print('initial arr', arr)
-# print(merge_sort(arr))
+arrB.extend(arrA)
+arr = arrB
+print('initial arr', arr)
+print(merge_sort(arr))
 
 
 # STRETCH: implement an in-place merge sort algorithm
