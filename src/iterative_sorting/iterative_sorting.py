f784d1b231fe67af0c63c62e6cac80da40023e41
Erin Koen <erin.koen@gmail.com>
2019-06-04 10:57:48 -0400

    bubble sort fixed

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -15,38 +15,32 @@ def selection_sort(arr):
         
         # TO-DO: swap
         # once the index of the smallest item in the list has been found, assign its value to arr[i] from the outer loop
-
         big = arr[i]
         small = arr[smallest_index]
         arr[i] = small
         arr[smallest_index] = big
-
-
 
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
-    for i in range(0, len(arr)-1):
+    sorting = True
+    while sorting:
+        # optimistically assumes that every loop of this while loop will the the last
+        sorting = False 
         # set index variables to compare to eachother
-        current_index = i
-        next_index = current_index + 1
-
+        
+        # need to figure out how to restart the loop at index 0
         # run the comparison through the loop till the last item
-        while next_index <= len(arr) - 1 :
+        for i in range(0, len(arr)-1):
             # compare 
-            if arr[current_index] > arr[next_index]:
+            if arr[i] > arr[i+1]:
                 # swap values and increment value of next_index variable
-                big = arr[current_index]
-                little = arr[next_index]
-                arr[current_index] = little
-                arr[next_index] = big
-                next_index += 1
-            elif arr[current_index] < arr[next_index]:
-                # if no swap needed, just increment value of next_index variables
-                next_index += 1
-                
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                # keep the while loop running
+                sorting = True
+
     return arr
 
 print (bubble_sort([1, 5, 8, 4, 2, 9, 6, 0, 3, 7]))
