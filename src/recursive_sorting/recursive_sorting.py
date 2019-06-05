ec7449a8281a19a136289122b35dceaf26244d2a
ed.chin.git <edgardochin@gmail.com>
2019-06-05 09:36:28 -0500

    Sorting : MVP2 completed

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,17 +1,39 @@
-# TO-DO: complete the helpe function below to merge 2 sorted arrays
-def merge( arrA, arrB ):
+# TO-DO: complete the helper function below to merge 2 sorted arrays
+def merge(arrA, arrB):
+    if len(arrA) == 0:
+        return arrB
+    if len(arrB) == 0:
+        return arrA
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
-    # TO-DO
-    
+
+    idx_a, idx_b = 0, 0
+    for i in range(elements):
+        # if either array is empty, return the other
+        if len(arrA) == idx_a:
+            for j in range(idx_b, len(arrB)):
+                merged_arr[i + j - idx_b] = arrB[j]
+            return merged_arr
+        elif len(arrB) == idx_b:
+            for j in range(idx_a, len(arrA)):
+                merged_arr[i + j - idx_a] = arrA[j]
+            return merged_arr
+        # add element in the right order
+        if arrA[idx_a] < arrB[idx_b]:
+            merged_arr[i] = arrA[idx_a]
+            idx_a += 1
+        else:
+            merged_arr[i] = arrB[idx_b]
+            idx_b += 1
     return merged_arr
 
 
-# TO-DO: implement the Merge Sort function below USING RECURSION
-def merge_sort( arr ):
-    # TO-DO
-
-    return arr
+# Merge Sort function USING RECURSION
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr  # if 1 element in the array return it
+    midpoint = len(arr) // 2
+    return merge(merge_sort(arr[:midpoint]), merge_sort(arr[midpoint:]))
 
 
 # STRETCH: implement an in-place merge sort algorithm
