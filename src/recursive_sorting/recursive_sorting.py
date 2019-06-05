7859ff93ec2212b1e6d19c0229073fc8f219df1b
Carlos Gutierrez <chgutier@gmail.com>
2019-06-05 01:15:45 -0500

    Not quite MVP for recursive sorting

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,17 +1,28 @@
-# TO-DO: complete the helpe function below to merge 2 sorted arrays
-def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
+# TO-DO: complete the helper function below to merge 2 sorted arrays
+def merge(arrA, arrB):
+    elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
     # TO-DO
-    
+    if arrA > arrB:
+        merged_arr[len(arrA):] = arrA
+        merged_arr[:len(arrB)] = arrB
     return merged_arr
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
-def merge_sort( arr ):
+def merge_sort(arr):
     # TO-DO
+    if len(arr) < 2: # base case
+        return arr
+    else:
+        half = len(arr) // 2
+        # print(half)
+        left = [el for el in arr[:half]]
+        # print(left)
+        right = [el for el in arr[half:]]
+        # print(right)
 
-    return arr
+    return merge_sort(left), merge_sort(right)
 
 
 # STRETCH: implement an in-place merge sort algorithm
