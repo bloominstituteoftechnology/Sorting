42d00fd68e48a2ace3e2dc04b47453620410758b
Robert Billington <chillingtonbillington@gmail.com>
2019-06-07 10:05:44 -0500

    Completed first half of merge sorts. All arrays recursively calling until only one item is left. I don't know how to combine them???

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,3 +1,5 @@
+
+
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
 def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
@@ -8,12 +10,28 @@ def merge( arrA, arrB ):
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
-def merge_sort( arr ):
-    # TO-DO
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    middle = len(arr) // 2
+    print(f"Middle: {middle}")
+    first_half = []
+    second_half = []
+    print(arr[middle])
+    for i in range(0, middle):
+        first_half.append(arr[i])
+        print(first_half)
+    for i in range(middle, len(arr)):
+        second_half.append(arr[i])
+        print(second_half)
+    merge_sort(first_half)
+    merge_sort(second_half)
+          
 
     return arr
 
-
+merge_sort([2, 9, 4, 16, 3, 7, 10])
 # STRETCH: implement an in-place merge sort algorithm
 def merge_in_place(arr, start, mid, end):
     # TO-DO
