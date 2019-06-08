2d7fc6586a0ef13769552660b1346f1c7e35929b
Emmanium <pradoe.ep@gmail.com>
2019-06-08 11:36:44 -0600

    filled out merge sort method

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -3,13 +3,26 @@ def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
     # TO-DO
-    curr_a = 0
-    curr_b = 0
+    a = 0
+    b = 0
 
-    for i in range(0, elements):
-        if curr_a >= len(arrA):
-            merged_arr[i] = arrB[curr_b]
-            curr_b += 1
+    for i in range(elements):
+        # If elements from arrA have been merged
+        if a >= len(arrA):
+            # put next element in arrB into array
+            merged_arr[i] = arrB[b]
+            b += 1
+        # if elements from arrB have been merged
+        elif b >= len(arrB):
+            merged_arr[i] = arrA[a]
+            a += 1
+        # if index of a is smaller than b
+        elif arrA[a] < arrB[b]:
+            merged_arr[i] = arrA[a]
+            a += 1
+        else:
+            merged_arr[i] = arrB[b]
+            b += 1
 
     return merged_arr
 
@@ -17,7 +30,10 @@ def merge(arrA, arrB):
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort(arr):
     # TO-DO
-
+    if len(arr) > 1:
+        left = merge_sort(arr[:len(arr) // 2])
+        right = merge_sort(arr[len(arr) // 2:])
+        arr = merge(left, right)
     return arr
 
 
