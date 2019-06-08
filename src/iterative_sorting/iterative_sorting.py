af29b497d6ef699f076c3240ebd245239e271007
Jonathan Brock <jbrock1981@gmail.com>
2019-06-08 09:25:13 -0500

    bubble_sort completed

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -6,22 +6,24 @@ def selection_sort( arr ):
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-        for j in range(i + 1, len(arr)):
+        for j in range(i, len(arr)):
             if arr[j] < arr[smallest_index]:
                 smallest_index = j
         # TO-DO: swap
-                arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
-    print(arr)
+        arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
-
+    for i in range(0, len(arr) - 1):
+        for j in range(0, len(arr) -1 -i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
     return arr
 
 
-# STRETCH: implement the Count Sort function below
-def count_sort( arr, maximum=-1 ):
+# # STRETCH: implement the Count Sort function below
+# def count_sort( arr, maximum=-1 ):
 
-    return arr
+#     return arr
