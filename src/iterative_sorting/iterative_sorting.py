d3bb67b6171e9da5b852c0b0c64fffe8730b5b7d
Kendra Williams <kmonique.williams@gmail.com>
2019-06-10 21:34:57 -0400

    corrected interative failed test

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -6,7 +6,7 @@ def selection_sort( arr ):
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-        for j in range(i+1, len(arr) - 1):
+        for j in range(i+1, len(arr)):
             if arr[smallest_index] > arr[j]:
                 smallest_index = j
         # TO-DO: swap
