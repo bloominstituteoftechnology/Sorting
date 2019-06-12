3f247a30be4b4a9bfc87074a4d95e905eef9b7e2
Jason Hedrick <jasonleehedrick@gmail.com>
2019-06-11 20:19:43 -0700

    A bit of help from Elissa and throwing in a secondary check if there is an empty list passed to just return. Now the test cases work.

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,44 +1,40 @@
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
 def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
-    merged_arr = [0] * elements
+    merged_arr = []
     # TO-DO
-    i = j = k = 0
-    while i < len(arrA) and j < len(arrB):
-        if arrA[i] < arrB[j]:
-            merged_arr[k] = arrA[i]
+    i = j = 0
+    while elements > 0:  
+        if i >= len(arrA):
+            merged_arr.append(arrB[j])
+            j += 1
+        elif j >= len(arrB):
+            merged_arr.append(arrA[i])
+            i += 1
+        elif arrA[i] < arrB[j]:
+            merged_arr.append(arrA[i])
             i += 1
         else:
-            merged_arr[k] = arrB[j]
+            merged_arr.append(arrB[j])
             j += 1
-        k += 1
-        
-    # fall through incase arrA or arrB had a larger array size
-    while i < len(arrA):
-        merged_arr[k] = arrA[i]
-        i += 1
-        k += 1
-    while j < len(arrB):
-        merged_arr[k] = arrB[j]
-        j += 1
-        k += 1
+        elements -= 1
+    
     return merged_arr
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
-    if len(arr) == 1:
-        print(f'exiting merge sort')
+    if len(arr) == 1 or arr == []:
+        #print(f'exiting merge sort, {arr}')
         return arr
 
-    print(f'inside merge_sort arr = {arr}')
+    #print(f'inside merge_sort arr = {arr}')
     mid = len(arr) // 2
-    arr1 = arr[:mid]
-    arr2 = arr[mid:]
-    arr = merge(merge_sort(arr1), merge_sort(arr2))
-
-    return arr
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+    return merge(left, right)
+    #arr = merge(merge_sort(arr1), merge_sort(arr2))
 
 print(merge_sort([4, 6, 2, 3, 1, 5]))
 
