02e4553cb668609b8c8e7c7363ef57deda95e30e
vy3191 <venkyafhs@gmail.com>
2019-06-30 18:37:56 -0400

    Competed binary_search in searching

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -10,14 +10,20 @@ def linear_search(arr, target):
 
 # STRETCH: write an iterative implementation of Binary Search 
 def binary_search(arr, target):
-
   if len(arr) == 0:
-    return -1 # array empty
-    
+    return -1 # array empty    
   low = 0
   high = len(arr)-1
-
   # TO-DO: add missing code
+  while low <= high:
+    middle = low + (high-low) // 2
+  
+    if target < arr[middle]:
+      high = middle-1
+    elif target > arr[middle]:
+      low = middle+1
+    else:
+      return middle       
 
   return -1 # not found
 
