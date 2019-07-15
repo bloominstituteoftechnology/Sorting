d457e8ac86e68472a259b469dbfa1045fabb2c77
Winston <wxl51@yahoo.com>
2019-07-15 17:24:06 -0500

    added stretch count_sort() to iterative_sorting.py

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -30,5 +30,22 @@ def bubble_sort( arr ):
 
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
+    #print(arr)
+    if len(arr) < 1:
+        return([])
+    if min(arr) < 0:
+        return('Error, negative numbers not allowed in Count Sort')
+    d = dict()
+    for key in range(len(arr)):
+        if arr[key] in d:
+            d[arr[key]] += 1
+        else:
+            d[arr[key]] = 1
+    arr = []
+    for i in range(len(d)):
+        key = min(d.keys())
+        for j in range(d.pop(key)):
+            arr.append(key)
 
     return arr
+#print(count_sort([4,6,5,6,3,2,5,3,2,1]))
