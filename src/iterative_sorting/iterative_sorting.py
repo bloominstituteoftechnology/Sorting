c5901a89c1fa7ce035f11d04e368e0a8f3786b63
Divya Nair <divnairs@gmail.com>
2019-07-15 15:26:31 -0700

    finished selection & bubble sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,30 +1,49 @@
-# TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
-    # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
+def selection_sort(arr):
+    # arr: [50, 20, 10, 30, 40]
+    # Find minimum element in arr[0,1,2,3,4] and swap it with arr[0]: 
+    # 10, 20, 50, 30, 40
 
+    # Find minimum element in arr[1,2,3,4] and swap it with arr[1]:
+    # 10, 20, 50, 30, 40
 
+    # Find minimum element in arr[2,3,4] and swap it with arr[2]:
+    # 10, 20, 30, 50, 40
 
-        # TO-DO: swap
-
-
-
-
+    # Find minimum element in arr[3,4] and swap it with arr[3]:
+    # 10, 20, 30, 40, 50
+    for i in range(len(arr)):
+        min_val = arr[i]
+        ith_val = arr[i]
+        min_index = i
+        for j in range(i+1, len(arr)):
+            if min_val > arr[j]:
+                min_val = arr[j]
+                min_index = j
+        arr[i] = min_val
+        arr[min_index] = ith_val
     return arr
 
+print("Selection:", selection_sort([50, 20, 10, 30, 40]))
 
-# TO-DO:  implement the Bubble Sort function below
-def bubble_sort( arr ):
 
+def bubble_sort(arr):
+    # Compare each element to the next:
+        # if 1st > 2nd, then swap
+        # if 2nd > 3rd, then swap
+        # repeat for all elements
+        # larger elements begin to "bubble" towards the end 
+    # Repeat comparing and swapping until the array is sorted
+    for i in range(len(arr)):
+        for j in range(len(arr)-1):
+            if arr[j] > arr[j+1]:
+                jth_val = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = jth_val
     return arr
 
+print("Bubble:", bubble_sort([50, 20, 10, 30, 40]))
 
-# STRETCH: implement the Count Sort function below
-def count_sort( arr, maximum=-1 ):
 
+# STRETCH
+def count_sort(arr, maximum=-1):
     return arr
