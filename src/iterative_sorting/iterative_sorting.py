13cd0e9be7880024985c6f6e3234f14ec53c02c9
cori-peck <cori.peck@ymail.com>
2019-08-12 18:20:42 -0500

    passing ine test now, bubbleup. switched to following the lec example don't like the variables

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -19,15 +19,11 @@ def selection_sort( arr ):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
-    swapped = True
-
-    while swapped:
-        swapped = False
-        
-        for i in range(len(arr)-1):
-            if arr[i] > arr[i+1]:
-                arr[i], arr[i+1] = arr[i+1], arr[i]
-                swapped = True
+    for i in range(len(arr)):
+        j = i
+        while j > 0 and arr[j] < arr[j-1]:
+            arr[j], arr[j-1] = arr[j-1], arr[j]
+            j -= 1
 
     return arr
 
