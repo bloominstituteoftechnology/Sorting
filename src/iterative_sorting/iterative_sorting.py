45dc3a31405a0d4e01c9ed0cfe434af9fc666544
cori-peck <cori.peck@ymail.com>
2019-08-13 14:06:33 -0500

    still failing selection sort, I give up

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,25 +1,29 @@
 # TO-DO: Complete the selection_sort() function below 
+arr = [64, 25, 12, 22, 11]
 def selection_sort( arr ):
     # loop through n-1 elements
-    for i in range(len(arr)):  #iterate over list to find smallest item, set that as current index
-        #cur_index = i
-        smallest_index = i
+    for i in range(0, len(arr) - 1):  #iterate over list to find smallest item, set that as current index
+        cur_index = i
+        smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-        for n in range(i + 1, len(arr)):  #iterate again to look at grab the item next to current index
+        for n in range(cur_index, len(arr)):  #iterate again to look at grab the item next to current index
             if arr[n] < arr[smallest_index]:  #compare, if next item is less then current index
                 smallest_index = n              #set next item as the new current index by swap
 
         # TO-DO: swap
-            arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
+            arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]
 
 
     return arr
 
+selection_sort(arr)
+print(arr)
+
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
-    for i in range(len(arr)):
+    for i in range(1, len(arr)):
         j = i           #I think this is the currect position if I'm understanding
         while j > 0 and arr[j] < arr[j-1]:        # checking each element to see if it is less
             arr[j], arr[j-1] = arr[j-1], arr[j]    #this is the actual swap
