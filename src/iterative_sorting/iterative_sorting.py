a81316c67f93b2ee90ffc04da7b5aafeed736d4a
Angel <angelbuenrostro@outlook.com>
2019-08-13 23:58:22 -0400

    Complete Bubble Sort Algorithm - Test Passed

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -17,7 +17,12 @@ def selection_sort( arr ):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
-
+    for i in range (len(arr) - 1, 0, -1):
+        for j in range (i):
+            if arr[j] > arr[j+1]:
+                temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = temp
     return arr
 
 
