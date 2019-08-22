1ea31c3537bc4f25b354a5e66ea57d93518d99bc
Arpita Sinha <sinha.arpita@gmail.com>
2019-08-22 09:20:40 -0700

    MVP AND ALL THE STRETCHES

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,30 +1,69 @@
-# TO-DO: Complete the selection_sort() function below 
+ # TO-DO: Complete the selection_sort() function below
+#arr=[5,3,8,2,1]
 def selection_sort( arr ):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
+        min = arr[i]
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-             
-
-
+        for j in range (i+1,len(arr)):
+            if arr[j] <min :
+               min= arr[j]
+               cur_index= j
 
         # TO-DO: swap
-
-
-
+        temp= arr[i]
+        arr[i] = arr[cur_index]
+        arr[cur_index]= temp
 
     return arr
+
+print ("Sorted array",selection_sort([5,3,8,2,1]))
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
+    # outer loop that will decide how many times the process will be repeated, the number of passes, it will be len(arr-1
+    for i in range(0,len(arr)-1):
+        swap=0
+        #get inside the inner for loop which will compare the elements and if they are not at the right position it will swap
+        for j in range (0,len(arr)-1-i):
+            if arr[j]>arr[j+1]:
+                temp=arr[j]
+                arr[j]=arr[j+1]
+                arr[j+1]=temp
+                swap=1
+        if swap==0:
+           break
 
     return arr
+print ("Sorted array",bubble_sort([5,3,8,2,1]))
 
 
 # STRETCH: implement the Count Sort function below
-def count_sort( arr, maximum=-1 ):
+def count_sort( arr, maximum=-1):
 
-    return arr
+    d = {}
+    minNum = min(arr)
+    maxNum = max(arr)
+
+    #Go through array and count frequency of each element
+    for i in arr:
+        if i in d:
+            d[i] += 1
+        else:
+            d[i] = 1
+
+    result = []
+    for i in range(minNum, maxNum + 1):#For each number possible in range
+        if i in d: #Check if we have seen this number. d[i] represents number of times i appeared
+            while d[i]: #We need to append i di] times
+                result.append(i)
+                d[i] -= 1 #Reduce frequence.
+
+
+    return result
+
+print ("Sorted array",count_sort([5,3,8,2,5,2,1,3]))
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,11 +1,17 @@
 # STRETCH: implement Linear Search				
 def linear_search(arr, target):
-  
+  for i in range(0,len(arr)):
+    if arr[i]== target:
+      print("Found target")
+      return True
+    print (i, "not found")
+
+  return False
   # TO-DO: add missing code
 
-   return -1   # not found
+      # not found
 
-
+print(linear_search([1,7,9,4,3],3))
 # STRETCH: write an iterative implementation of Binary Search 
 def binary_search(arr, target):
 
@@ -15,16 +21,46 @@ def binary_search(arr, target):
   low = 0
   high = len(arr)-1
 
+  while low<=high:
+    middle = (low + high) // 2
+    if target == arr[middle]:
+       print("got the target")
+       return True
+
+    if target > arr[middle]:
+      low =middle+ 1
+      print("this is low,"+str(low))
+    else:
+      high = middle-1
+      print("this is high"+str(high))
+
+
   # TO-DO: add missing code
 
   return -1 # not found
 
+print("Btree",binary_search([1,9,12,18,24,45,47,90],12))
 
 # STRETCH: write a recursive implementation of Binary Search 
 def binary_search_recursive(arr, target, low, high):
-  
+  #low=0
+  #high =len(arr)-1
+
   middle = (low+high)//2
+  print (middle)
+  print(arr[middle])
 
   if len(arr) == 0:
-    return -1 # array empty
+     return -1 # array empty
+  if target ==arr[middle]:
+     return True
+  if target>arr[middle]:
+     #low=middle+1
+     return binary_search_recursive(arr[middle+1:],target)
+  else:
+     return binary_search_recursive(arr[:middle],target)
   # TO-DO: add missing if/else statements, recursive calls
+  return -1  # not found
+
+
+print("BreEcursivetree",binary_search_recursive([1, 9, 12, 18, 24, 45, 47, 90,100],90,0,8))
