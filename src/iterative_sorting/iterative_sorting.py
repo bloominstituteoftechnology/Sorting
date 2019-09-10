d41981ea7d363b6d3cbf2ba3c4a480bf3a9d0a1d
jimpakdev <jamesjiminpak@gmail.com>
2019-09-10 10:18:17 -0700

    brady's way

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -6,7 +6,7 @@ def selection_sort( arr ):
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-        for j in range(i+1, len(arr)):
+        for j in range(i, len(arr)):
             if arr[smallest_index] > arr[j]:
                 smallest_index = j
 
@@ -24,14 +24,26 @@ def selection_sort( arr ):
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
 
-    for i in range( len(arr)-1, 0, -1 ):
+    # for i in range( len(arr)-1, 0, -1 ):
 
-        for j in range(i):
+    has_swapped = True
 
-            if arr[j] > arr[j+1]:
-                temp = arr[j]
-                arr[j] = arr[j+1]
-                arr[j+1] = temp
+    while has_swapped:
+        has_swapped = False
+
+        for i in range(0, len(arr) - 1):
+
+            if arr[i] > arr[i+1]:
+
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                has_swapped = True
+
+        # for j in range(i):
+
+        #     if arr[j] > arr[j+1]:
+        #         temp = arr[j]
+        #         arr[j] = arr[j+1]
+        #         arr[j+1] = temp
     
     return arr
 
