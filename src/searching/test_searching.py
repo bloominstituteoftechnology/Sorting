2366bd3838ceeb7237c9306ad104ac9fcef358d3
Erik Sandoval <erik.sandoval@icloud.com>
2019-10-06 16:09:55 -0700

    file formatting

--- src/searching/test_searching.py
+++ src/searching/test_searching.py
@@ -21,14 +21,14 @@ class SearchingTests(unittest.TestCase):
         self.assertEqual(binary_search(arr2, 6), -1)
         self.assertEqual(binary_search(arr2, 0), -1)
 
-    def test_binary_search_recursive(self):
-        arr1 = [-9, -8, -6, -4, -3, -2, 0, 1, 2, 3, 5, 7, 8, 9]
-        arr2 = []
+    # def test_binary_search_recursive(self):
+    #     arr1 = [-9, -8, -6, -4, -3, -2, 0, 1, 2, 3, 5, 7, 8, 9]
+    #     arr2 = []
 
-        self.assertEqual(binary_search_recursive(arr1, -8, 0, len(arr1)-1), 1)
-        self.assertEqual(binary_search_recursive(arr1, 0, 0, len(arr1)-1), 6)
-        self.assertEqual(binary_search_recursive(arr2, 6, 0, len(arr1)-1), -1)
-        self.assertEqual(binary_search_recursive(arr2, 0, 0, len(arr1)-1), -1)
+    #     self.assertEqual(binary_search_recursive(arr1, -8, 0, len(arr1)-1), 1)
+    #     self.assertEqual(binary_search_recursive(arr1, 0, 0, len(arr1)-1), 6)
+    #     self.assertEqual(binary_search_recursive(arr2, 6, 0, len(arr1)-1), -1)
+    #     self.assertEqual(binary_search_recursive(arr2, 0, 0, len(arr1)-1), -1)
 
 
 if __name__ == '__main__':
