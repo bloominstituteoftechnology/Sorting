144f6b136ff6e9828b47009d7dc0ddd4466ea571
pedrolmr <pedroluismontesinosrivera@gmail.com>
2019-10-07 16:26:09 -0400

    added bubble sort stretch goal implementation

--- project/iterative_sorting.py
+++ project/iterative_sorting.py
@@ -32,13 +32,11 @@ def bubble_sort( arr ):
     for i in arr[::-1]:
         for j in range(0, len(arr) - 1):
             if arr[j] > arr[j + 1]:
-                # print(arr, arr[j], arr[j + 1])
                 temp = arr[j]
                 arr[j] = arr[j + 1]
                 arr[j + 1] = temp
     return arr
 print(bubble_sort([3, 2, 41, 34, 22]))
-
 
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
