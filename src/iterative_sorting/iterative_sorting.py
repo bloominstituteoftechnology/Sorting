7d5abc51d49b5ae2e788ceb7225ae9788e047c1a
basilcea <ogbonnabasil3@gmail.com>
2019-10-07 15:56:52 +0100

    fixed sorting algorithms

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -5,6 +5,10 @@ def selection_sort( arr ):
         cur_index = i
         smallest_index = cur_index
         # TO-DO: find next smallest element
+        for j in range(cur_index , len(arr)):
+            if(arr[smallest_index] > arr[j]):
+                smallest_index = j
+        (arr[cur_index], arr[smallest_index]) = (arr[smallest_index], arr[cur_index])
         # (hint, can do in 3 loc) 
              
 
@@ -20,7 +24,16 @@ def selection_sort( arr ):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
-
+    # print(arr)
+    sorted = False
+    while not sorted:
+        swap = False
+        for i in range(0 , len(arr)-1):
+            if(arr[i] > arr[i+1]):
+                (arr[i], arr[i+1]) = (arr[i+1], arr[i])
+                swap = True
+        if not swap :
+            sorted =True
     return arr
 
 
