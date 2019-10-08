8b4e4a57ccb35c9006671d443f76086a187d1d89
pedrolmr <pedroluismontesinosrivera@gmail.com>
2019-10-08 17:27:38 -0400

    fixed bug in merge_sort function

--- project/recursive_sorting.py
+++ project/recursive_sorting.py
@@ -24,8 +24,8 @@ def merge( arrA, arrB ):
 ### recursive sorting function
 def merge_sort( arr ):
     if len( arr ) > 1:
-        left = merge_sort( arr[ 0 : len( arr ) / 2 ] )
-        right = merge_sort( arr[ len( arr ) / 2 : ] )
+        left = merge_sort( arr[0:len( arr ) // 2 ] )
+        right = merge_sort( arr[ len( arr ) // 2 : ] )
         arr = merge( left, right )   # merge() defined later
     return arr
 
@@ -33,53 +33,16 @@ def merge_sort( arr ):
 # STRETCH: implement an in-place merge sort algorithm
 def merge_in_place(arr, start, mid, end):
     # TO-DO
+    pass
 
-    return arr
+    # return arr
 
 def merge_sort_in_place(arr, l, r): 
     # TO-DO
+    pass
 
-    return arr
+    # return arr
 
-
-# TO-DO: implement the Quick Sort function below
-# def quick_sort(arr, low=None, high=None):
-#     if len(arr) <= 1:
-#         return arr
-#     elif len(arr) == 2:
-#         if arr[0] > arr[1]:
-#             arr.insert(0, arr.pop())
-#         return arr
-#     for i in arr:
-#         if i < arr[0] and low is None:
-#             low = []
-#             low.append(i)
-#         elif i > arr[0] and high is None:
-#             high = []
-#             high.append(i)
-#     return quick_sort(low) + [arr[0]] + quick_sort(high)
-
-# arr = [20, 2, 1, 89, 43, 67]
-# print(quick_sort(arr))
-
-def quick_sort(arr):
-    low = []
-    high = []
-    if len(arr) <= 1:
-        return arr
-    elif len(arr) == 2:
-        if arr[0] > arr[1]:
-            arr.insert(0, arr.pop())
-        return arr
-    for i in arr:
-        if i < arr[0]:
-            low.append(i)
-        elif i > arr[0]:
-            high.append(i)
-    return quick_sort(low) + [arr[0]] + quick_sort(high)
-
-arr = [20, 2, 1, 89, 43, 67]
-print(quick_sort(arr))
 # STRETCH: implement the Timsort function below
 # hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
 def timsort( arr ):
--- project/tests.py
+++ project/tests.py
@@ -1,7 +1,7 @@
 import unittest
 from searching import *
 from iterative_sorting import * 
-from recursive_sorting import merge_sort, merge_sort_in_place, quick_sort
+from recursive_sorting import merge_sort, merge_sort_in_place
 
 class SortingTest(unittest.TestCase):
 
@@ -75,16 +75,16 @@ class SortingTest(unittest.TestCase):
     #     self.assertEqual(count_sort(arr3), "Error, negative numbers not allowed in Count Sort")
 
 
-    # def test_merge(self):
-    #     arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
-    #     arr2 = []
-    #     arr3 = [2]
-    #     arr4 = [0, 1, 2, 3, 4, 5]
+    def test_merge(self):
+        arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+        arr2 = []
+        arr3 = [2]
+        arr4 = [0, 1, 2, 3, 4, 5]
 
-    #     self.assertEqual(merge_sort(arr1), [0,1,2,3,4,5,6,7,8,9])
-    #     self.assertEqual(merge_sort(arr2), [])
-    #     self.assertEqual(merge_sort(arr3), [2])
-    #     self.assertEqual(merge_sort(arr4), [0,1,2,3,4,5])
+        self.assertEqual(merge_sort(arr1), [0,1,2,3,4,5,6,7,8,9])
+        self.assertEqual(merge_sort(arr2), [])
+        self.assertEqual(merge_sort(arr3), [2])
+        self.assertEqual(merge_sort(arr4), [0,1,2,3,4,5])
 
 
     # def test_merge_in_place(self):
