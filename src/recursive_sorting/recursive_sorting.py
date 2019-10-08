d995689e7aa2e5e1cd6fedc33d2745ac05c8bae4
Rice_from_data <nedderlander@gmail.com>
2019-10-08 13:15:07 -0700

    day2 complete

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -2,16 +2,49 @@
 def merge(arrA, arrB):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
-    # TO-DO
     
+    #counters for lengths of all 3 arrays
+    i, j, k = 0, 0, 0
+
+    #traverse both arrays
+    while i < len( arrA ) and j < len ( arrB ):
+
+        # check if the current element of A is smaller than current element of B
+        if arrA[i] < arrB[j]:
+            merged_arr[k] = arrA[i]
+            k += 1
+            i += 1
+        else:
+            merged_arr[k] = arrB[j]
+            k += 1
+            j += 1
+    #iterate through remaining elements of each array
+    while j < len( arrB ):
+        merged_arr[k] = arrB[j]
+        k += 1
+        j += 1
+    
+    while i < len( arrA ):
+        merged_arr[k] = arrA[i]
+        k += 1
+        i += 1
+    
+    # print(f'i {i}, j {j}, k {k}')
     return merged_arr
+
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
-    # TO-DO
+    # base case
+    if len( arr ) <= 1:
+        return arr
 
-    return arr
+    half = len( arr ) // 2
+    left = merge_sort(arr[:half])
+    right = merge_sort(arr[half:])
+
+    return merge(left, right)
 
 
 # STRETCH: implement an in-place merge sort algorithm
@@ -31,3 +64,30 @@ def merge_sort_in_place(arr, l, r):
 def timsort( arr ):
 
     return arr
+
+# quick sort
+
+# Polya's
+# 1 Understand the problem
+# 2 Devise a plan
+# 3 Carry out the plan
+# 4 Look Back
+
+# def quicksort(items):
+#     # 0. base case
+#     if len(items)<= 1:
+#         return items
+    
+#     # 1. Select the last element as the pivot
+#     pivot = items[-1]
+#     left = []
+#     right = []
+
+#     for i in range(len(items)-1):
+#         item = items[i]
+
+#         if item < pivot:
+#             left.append(item)
+#         else:
+#             right.append(item)
+#     return quicksort(left) + [pivot] + quicksort(right)
