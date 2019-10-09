58c6da195f6654aec245354a3259b246f49a5a62
basilcea <ogbonnabasil3@gmail.com>
2019-10-09 13:23:10 +0100

    fix merge sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,43 +1,37 @@
-# TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
+# TO-DO: Complete the selection_sort() function below
+def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
         # TO-DO: find next smallest element
-        for j in range(cur_index , len(arr)):
+        for j in range(cur_index + 1, len(arr)):
             if(arr[smallest_index] > arr[j]):
                 smallest_index = j
-        (arr[cur_index], arr[smallest_index]) = (arr[smallest_index], arr[cur_index])
-        # (hint, can do in 3 loc) 
-             
-
-
-
+        (arr[cur_index], arr[smallest_index]) = (
+            arr[smallest_index], arr[cur_index])
+        # (hint, can do in 3 loc)
         # TO-DO: swap
-
-
-
 
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
-def bubble_sort( arr ):
+def bubble_sort(arr):
     # print(arr)
     sorted = False
     while not sorted:
         swap = False
-        for i in range(0 , len(arr)-1):
+        for i in range(0, len(arr)-1):
             if(arr[i] > arr[i+1]):
                 (arr[i], arr[i+1]) = (arr[i+1], arr[i])
                 swap = True
-        if not swap :
-            sorted =True
+        if not swap:
+            sorted = True
     return arr
 
 
 # STRETCH: implement the Count Sort function below
-def count_sort( arr, maximum=-1 ):
+def count_sort(arr, maximum=-1):
 
     return arr
--- src/iterative_sorting/test_iterative.py
+++ src/iterative_sorting/test_iterative.py
@@ -3,16 +3,16 @@ import random
 from iterative_sorting import *
 
 class IterativeSortingTest(unittest.TestCase):
-    # def test_selection_sort(self):
-    #     arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
-    #     arr2 = []
-    #     arr3 = [0, 1, 2, 3, 4, 5]
-    #     arr4 = random.sample(range(200), 50)
+    def test_selection_sort(self):
+        arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+        arr2 = []
+        arr3 = [0, 1, 2, 3, 4, 5]
+        arr4 = random.sample(range(200), 50)
 
-    #     self.assertEqual(selection_sort(arr1), [0,1,2,3,4,5,6,7,8,9])
-    #     self.assertEqual(selection_sort(arr2), [])
-    #     self.assertEqual(selection_sort(arr3), [0,1,2,3,4,5])
-    #     self.assertEqual(selection_sort(arr4), sorted(arr4))
+        self.assertEqual(selection_sort(arr1), [0,1,2,3,4,5,6,7,8,9])
+        self.assertEqual(selection_sort(arr2), [])
+        self.assertEqual(selection_sort(arr3), [0,1,2,3,4,5])
+        self.assertEqual(selection_sort(arr4), sorted(arr4))
 
     def test_bubble_sort(self):
         arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
