fede49318c14e79af239639f8d697314159ff535
hectorsvill <hectorstevenvillasano@gmail.com>
2019-10-08 22:51:26 -0700

    split

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,12 +1,18 @@
+l = [1, 5, 8, 4, 2, 9, 6, 0, 10, 3, 7]
+
+
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
 def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
     # TO-DO
-    
+    print(f"elements: {elements} - merged_arr {merged_arr}")
+
     return merged_arr
 
-l = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+
+
+# x = merge(l[:4], l[4:])
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
@@ -15,9 +21,26 @@ def merge_sort( arr ):
     if (len(arr) == 1):
         return arr
 
+    half_len = len(arr)//2
+
+    # get first half
+    arr1 = l[:half_len]
+
+    # get second half
+    arr2 = l[half_len:]
+
+    print(f"{arr1} - {arr2}")
 
     return arr
 
+
+
+# print(l)
+# print(l[:len(l)//2])
+# print(l[len(l)//2:])
+
+
+merge_sort(l)
 
 
 # STRETCH: implement an in-place merge sort algorithm
