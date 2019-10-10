85f20a208c9fe2ae483f20f16bd539a9e45983ff
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-10-09 21:44:42 -0700

    Update iterative_sorting.py

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,30 +1,43 @@
 # TO-DO: Complete the selection_sort() function below 
 def selection_sort( arr ):
     # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
+    for i in range(len(arr)):
+        smallest_index = i
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-             
-
-
-
+        for j in range(i + 1, len(arr)):
+            if arr[smallest_index] > arr[j]:
+                smallest_index = j
         # TO-DO: swap
-
-
-
+        arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
 
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
-
-    return arr
+    swapped = True 
+    while swapped: # while swapped is True, runnit
+        for i in range(len(arr)):
+            swapped = False # swapped is now Faaaaalse
+            for j in range(i + 1, len(arr)): # two loops but j is +1
+                if arr[i] > arr[j]: # if i > j
+                    arr[i], arr[j] = arr[j], arr[i] # switcheroo
+                    swapped = True # swappy is now true
+        return arr
 
 
 # STRETCH: implement the Count Sort function below
-def count_sort( arr, maximum=-1 ):
 
-    return arr
+def counting_sort(array, maxval):
+    """in-place counting sort"""
+    m = maxval + 1
+    count = [0] * m # init with zeros
+    for a in array:
+        count[a] += 1 # count occurences
+    i = 0
+    for a in range(m): # emit
+        for c in range(count[a]): # - emit 'count[a]' copies of 'a'
+            array[i] = a
+            i += 1
+    return (array,count)
