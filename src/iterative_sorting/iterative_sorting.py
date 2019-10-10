17538b335810181fe0efa32c91f12dc485d95852
hectorsvill <hectorstevenvillasano@gmail.com>
2019-10-10 07:34:52 -0700

    clean

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -24,53 +24,37 @@ def selection_sort( arr ):
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
-        #print(f"i:{i}")
-
         finished = True
         new_arr = arr[smallest_index + 1:]
-        for item_count in range(len(new_arr)):
 
+        for item_count in range(len(new_arr)):
             if new_arr[item_count] < arr[i]:
                 arr.insert(cur_index, arr.pop(cur_index + item_count + 1))
                 finished = False
 
-         #       print(f"arr{arr} \n")
-
         if arr[i] > arr[i+1]:
             arr[i], arr[i+1] = arr[i + 1], arr[i]
 
-
-        # print(f"{i} --->>>> {arr}")
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc)
-        # TO-DO: swap
-
     return arr
-
-arr = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
-
-# print(f"\nstart ->> {arr}\n\n")
-
-# a = selection_sort(arr)
-
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
     has_swapped = False
-    for _ in range(len(arr)):
-        for i in range(len(arr) - 1):
-            if arr[i] > arr[i + 1]:
-                arr[i], arr[i+1] = arr[i + 1], arr[i]
-                has_swapped = True
-                print(f"\n{arr}")
+
+    for i in range(len(arr) - 1):
+        if arr[i] > arr[i + 1]:
+            arr[i], arr[i+1] = arr[i + 1], arr[i]
+            has_swapped = True
+            print(f"\n{arr}")
+
     if has_swapped:
         return bubble_sort(arr)
     else:
         return arr
 
 
-x = bubble_sort(arr)
+
 
 # print(x)
 
