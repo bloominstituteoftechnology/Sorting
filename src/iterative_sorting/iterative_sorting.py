a2724c01824bd0634727327d8498eea982a7a5b7
Richard Scott Prins Jr <richardsprins@gmail.com>
2019-11-05 12:26:22 -0500

    finished selection sort and bubble sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,3 +1,5 @@
+test_list = [5,2,7,4,9,10,3,1,0,34]
+
 # TO-DO: Complete the selection_sort() function below 
 def selection_sort( arr ):
     # loop through n-1 elements
@@ -19,14 +21,21 @@ def selection_sort( arr ):
     return arr
 
 
-print(selection_sort([5,2,7,4,9,10,3,1,0,34]))
+print(selection_sort(test_list))
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
+    for indx in range(0, len(arr)):
+        for xndx in range(0, len(arr) - 1):
+            if arr[xndx] > arr[xndx + 1]:
+                tndx = arr[xndx]
+                arr[xndx] = arr[xndx + 1]
+                arr[xndx + 1] = tndx
+    
 
     return arr
 
-
+print(bubble_sort(test_list))
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
 
