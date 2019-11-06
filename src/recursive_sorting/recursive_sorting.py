7e30155cb9e06d77e54bed700480f3033090836b
ndoshi83 <49622764+ndoshi83@users.noreply.github.com>
2019-11-06 14:31:27 -0800

    Assignment 2

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -3,6 +3,19 @@ def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
     # TO-DO
+    ai = 0
+    bi = 0
+    for i in range(0,len(merged_arr)-1):
+        if arrA[ai] < arrB[bi]:
+            merged_arr[i] = arrA[ai]
+            ai += 1
+            print(ai)
+            print(bi)``
+        else:
+            merged_arr[i] = arrB[bi]
+            bi += 1
+            print(bi)
+            print(ai)
     
     return merged_arr
 
@@ -10,7 +23,15 @@ def merge( arrA, arrB ):
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
+    arrlen = len(arr)
+    if arrlen > 1:
+        arr1 = merge_sort(arr[:arrlen // 2])
+        arr2 = merge_sort(arr[arrlen // 2:])
 
+        print(arr1)
+        print(arr2)
+        arr = merge(arr1, arr2)
+    
     return arr
 
 
