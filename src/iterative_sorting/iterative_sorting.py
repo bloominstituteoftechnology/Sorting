b51b3e68dfd2d67ca5239c25f3ee9eb8f7f83150
Mr.T <rturgeon13@gmail.com>
2019-12-04 12:56:06 -0600

    created: selection sort
    fixed: bubble sort w/ proper return to pass test

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -22,24 +22,44 @@ def bubble_sort( arr ):
             print(arr)
 
         if noSwap == True:
-            return False
+            return arr
 
+    # return arr
+
+print(bubble_sort([2,4,5,1,8,5]))
+
+# TO-DO: Complete the selection_sort() function below 
+def selection_sort( arr ):
+    # V2
+    for i in range(len(arr)):
+        # Set minimum spot in array --> where you are going to swap to
+        min_index = i
+        
+        # loop through unsorted array 
+        for x in range(i+1, len(arr)):
+
+            # conditional to swap with current min index
+            if arr[min_index] > arr[x]:
+                min_index = x
+    
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+        print(arr)
+    
     return arr
-bubble_sort([1,6,9,3,10,4])
-
-# # TO-DO: Complete the selection_sort() function below 
-# def selection_sort( arr ):
-#     # loop through n-1 elements
-#     for i in range(0, len(arr) - 1):
-#         cur_index = i
-#         smallest_index = cur_index
-#         # TO-DO: find next smallest element
-#         # (hint, can do in 3 loc) 
-
-#         # TO-DO: swap
 
 
-#     return arr
+    # V1
+    # # loop through n-1 elements
+    # for i in range(0, len(arr) - 1):
+    #     cur_index = i
+    #     smallest_index = cur_index
+    #     # TO-DO: find next smallest element
+    #     # (hint, can do in 3 loc) 
+
+    #     # TO-DO: swap
+
+
+    # return arr
 
 # # STRETCH: implement the Count Sort function below
 # def count_sort( arr, maximum=-1 ):
