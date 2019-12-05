7060e5a4661ee9ab1ed1dba27e7f91c02e379ed7
David <digital0david@gmail.com>
2019-12-05 00:04:56 -0500

    smallest value returned

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,22 +1,36 @@
 # TO-DO: Complete the selection_sort() function below 
 def selection_sort( arr ):
+    first_value = arr[0]
+
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
+        original_value = 
+        
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-             
+        if arr[cur_index] < first_value:
+            
+            first_value = arr[cur_index]
+            smallest_index = cur_index
+            
+
+        
+           
+              
+       
 
 
 
         # TO-DO: swap
+    print(first_value)
+    
+    return print(arr)
 
+array = [10, 8, 5, 2, 7, 4, 3]
 
-
-
-    return arr
-
+selection_sort(array)
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
