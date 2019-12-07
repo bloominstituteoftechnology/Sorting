42d0759b2055c43c67f973a84f2cfdc4d48267e7
Reyaad Rafeek <reyaad510@gmail.com>
2019-12-07 14:33:49 -0500

    First part of merge sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -4,35 +4,35 @@ def selection_sort(arr):
     for i in range(0, len(arr) - 1):
         # Find minimum element
         cur_index = i
-        print("I am i in 1st loop:", cur_index)
+        # print("I am i in 1st loop:", cur_index)
         # have to not do len(arr) - 1 to access last element
         for j in range(i+1, len(arr)):
             # print("I am J: ", j)
-            print("J Loop 1. I am arr[cur_index]", arr[cur_index])
-            print("J Loop 2. I am arr[j]", arr[j])
+            # print("J Loop 1. I am arr[cur_index]", arr[cur_index])
+            # print("J Loop 2. I am arr[j]", arr[j])
             if arr[cur_index] > arr[j]:
                 cur_index = j  # Looping through values in array and setting cur_index to the array number that is the smallest
-                print("J Loop >. cur_index set to j", cur_index)
+                # print("J Loop >. cur_index set to j", cur_index)
 
         # Swap minimum element
-        print("I occur after j loop finishes then goes back to i loop -> j loop")
+        # print("I occur after j loop finishes then goes back to i loop -> j loop")
         # Current number you are on in the i for loop. Setting x to be first array number
         x = arr[i]
-        print("1. I am x", x)
-        print("2. I am arr[cur_index]", arr[cur_index])
+        # print("1. I am x", x)
+        # print("2. I am arr[cur_index]", arr[cur_index])
         # arr[cur_index] is the lowest number found and being set to current i position(being near beginning. So smallest going to front of array)
         # position arr[i] is being set to the lowest number found in j loop. Swap begins
         arr[i] = arr[cur_index]
-        print("3. I am arr[i] being set to to arr[cur_index]", arr[i])
+        # print("3. I am arr[i] being set to to arr[cur_index]", arr[i])
 
         arr[cur_index] = x  # index that had the smallest number being swapped
         # The index where the smaller number was is now getting the bigger number
-        print("4. ", arr[cur_index])
+        # print("4. ", arr[cur_index])
 
     return arr
 
 
-# selection_sort([5, 3, 4, 2, 1])
+selection_sort([5, 3, 4, 2, 1])
 
 
 # TO-DO:  implement the Bubble Sort function below
@@ -42,9 +42,9 @@ def bubble_sort(arr):
     x = len(arr)
 
     for i in range(x):
-        print("I am I: ", i)
+        # print("I am I: ", i)
         for j in range(0, x-i-1):  # range of 0, len(arr) - i - 1. Initially will be len(arr) - 0 - 1
-            print("I am j: ", j)
+            # print("I am j: ", j)
             if arr[j] > arr[j+1]:
                 # Line below is same as arr[j] = arr[j+1] and arr[j+1] = arr[j]
                 # Swapping the small and big numbers.
--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,17 +1,38 @@
-# TO-DO: complete the helpe function below to merge 2 sorted arrays
-def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
+# TO-DO: complete the helper function below to merge 2 sorted arrays
+def merge(arrA, arrB):
+    elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
     # TO-DO
-    
+    merged_arr = arrA + arrB
+    print(merged_arr)
+
     return merged_arr
 
 
+# merge([2, 2, 3], [2, 2, 3])
+
+
 # TO-DO: implement the Merge Sort function below USING RECURSION
-def merge_sort( arr ):
+def merge_sort(arr):
     # TO-DO
+    if len(arr) > 1:
+        middle = len(arr) // 2
+        left = arr[:middle]
+        right = arr[middle:]
+
+        # Recursive call on each half to divide them
+        merge_sort(left)
+        merge_sort(right)
+        # print("I am Middle: ", middle)
+        # print("I am the Left: ", left)
+        # print("I am the right: ", right)
+        # print("Printing mergeSort left: ", merge_sort(left))
+        # print("Printing mergeSort Right: ", merge_sort(right))
 
     return arr
+
+
+merge_sort([3, 7, 1, 5, 4])
 
 
 # STRETCH: implement an in-place merge sort algorithm
@@ -20,7 +41,8 @@ def merge_in_place(arr, start, mid, end):
 
     return arr
 
-def merge_sort_in_place(arr, l, r): 
+
+def merge_sort_in_place(arr, l, r):
     # TO-DO
 
     return arr
@@ -28,6 +50,6 @@ def merge_sort_in_place(arr, l, r):
 
 # STRETCH: implement the Timsort function below
 # hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
-def timsort( arr ):
+def timsort(arr):
 
     return arr
