e18bada2961d0cb25bac92c1aa7ba843e0f669b2
Diamond Purvis <purvisdiamond@gmail.com>
2019-12-07 15:11:49 -0500

    FInd next smallest element

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -6,6 +6,9 @@ def selection_sort( arr ):
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
+        for j in range(i + 1, len(arr)):
+            if arr[smallest_index] > arr[j]:
+                smallest_index = j
              
 
 
