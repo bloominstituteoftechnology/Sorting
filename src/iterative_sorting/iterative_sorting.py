eec5203336d63e706da01943c8f6695ceae9607a
Diamond Purvis <purvisdiamond@gmail.com>
2019-12-07 15:13:26 -0500

    Sort by order

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -9,7 +9,6 @@ def selection_sort( arr ):
         for j in range(i + 1, len(arr)):
             if arr[smallest_index] > arr[j]:
                 smallest_index = j
-             
 
 
 
@@ -20,13 +19,17 @@ def selection_sort( arr ):
 
 
 
-
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
-
+    for i in range(len(arr) - 1, 0, -1):
+        for j in range(i):
+            if arr[j] > arr[j+1]:
+                swap = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = swap
     return arr
 
 
