eacd4461d6beefc5d01a53b76da4d2f5f04b9a30
Rane <github@blasph.me>
2019-12-10 11:20:58 -0700

    Completed selection sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -4,16 +4,14 @@ def selection_sort( arr ):
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
 
+        for j in range(i, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
 
-
-        # TO-DO: swap
-
-
-
+        tmp_num = arr[cur_index]
+        arr[cur_index] = arr[smallest_index]
+        arr[smallest_index] = tmp_num
 
     return arr
 
