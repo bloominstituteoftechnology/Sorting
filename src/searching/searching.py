5ed946f038e4d9ff5f3a091c2f3e0f45499ccab2
nisaChampagne <50988313+nisaChampagne@users.noreply.github.com>
2019-12-09 23:56:34 -0500

    working in searching.py

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,24 +1,29 @@
-# STRETCH: implement Linear Search				
+#STRETCH: implement Linear Search				
 def linear_search(arr, target):
-  
-  # TO-DO: add missing code
-
-   return -1   # not found
-
+  index_length = len(arr)
+  for i in range(0, index_length):
+    if arr[i] == target:
+      return i
+  return -1   # not found
+print(linear_search([5,2,1,8,6,9], 8))
 
 # STRETCH: write an iterative implementation of Binary Search 
 def binary_search(arr, target):
-
-  if len(arr) == 0:
-    return -1 # array empty
-    
-  low = 0
-  high = len(arr)-1
-
-  # TO-DO: add missing code
-
-  return -1 # not found
-
+	first = 0
+	last = len(arr)-1
+	found = False
+	while( first<=last and not found):
+		middle = (first + last)//2
+		if arr[middle] == target :
+			found = True
+		else:
+			if target < arr[middle]:
+				last = middle - 1
+			else:
+				first = middle + 1
+	return found
+print(binary_search([1,2,3,5,8], 6))
+print(binary_search([1,2,3,5,8], 5))
 
 # STRETCH: write a recursive implementation of Binary Search 
 def binary_search_recursive(arr, target, low, high):
