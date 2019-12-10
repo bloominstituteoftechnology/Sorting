0b7b280eb39a7ca6a6258ff339b6d979b8a99883
Rane <github@blasph.me>
2019-12-10 11:35:41 -0700

    Completed bubble sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -18,6 +18,25 @@ def selection_sort( arr ):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
+    swapped = False
+    isSorting = True
+    index = 0
+
+    while isSorting:
+        if index >= len(arr) - 1:
+            if not(swapped):
+                return arr
+            else:
+                index = 0
+                swapped = False
+
+        if arr[index] > arr[index + 1]:
+            tmp_num = arr[index]
+            arr[index] = arr[index + 1]
+            arr[index + 1] = tmp_num
+            swapped = True
+
+        index += 1
 
     return arr
 
@@ -26,3 +45,8 @@ def bubble_sort( arr ):
 def count_sort( arr, maximum=-1 ):
 
     return arr
+
+
+arr = [2, 4, 6, 8, 1, 3]
+arr = bubble_sort(arr)
+print(arr)
