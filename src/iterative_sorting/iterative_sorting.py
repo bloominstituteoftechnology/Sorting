b7b87441dd1d04a48ac79451459b148c2a4169a4
Reyaad Rafeek <reyaad510@gmail.com>
2019-12-11 13:31:06 -0500

    Finished recursive kinda

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,38 +1,57 @@
-# TO-DO: Complete the selection_sort() function below
+# # TO-DO: Complete the selection_sort() function below
+
+# Type 1
+
+# def selection_sort(arr):
+#     # loop through n-1 elements
+#     for i in range(0, len(arr) - 1):
+#         # Find minimum element
+#         cur_index = i
+#         # print("I am i in 1st loop:", cur_index)
+#         # have to not do len(arr) - 1 to access last element
+#         for j in range(i+1, len(arr)):
+#             # print("I am J: ", j)
+#             # print("J Loop 1. I am arr[cur_index]", arr[cur_index])
+#             # print("J Loop 2. I am arr[j]", arr[j])
+#             if arr[cur_index] > arr[j]:
+#                 cur_index = j  # Looping through values in array and setting cur_index to the array number that is the smallest
+#                 # print("J Loop >. cur_index set to j", cur_index)
+
+#         # Swap minimum element
+#         # print("I occur after j loop finishes then goes back to i loop -> j loop")
+#         # Current number you are on in the i for loop. Setting x to be first array number
+#         x = arr[i]
+#         # print("1. I am x", x)
+#         # print("2. I am arr[cur_index]", arr[cur_index])
+#         # arr[cur_index] is the lowest number found and being set to current i position(being near beginning. So smallest going to front of array)
+#         # position arr[i] is being set to the lowest number found in j loop. Swap begins
+#         arr[i] = arr[cur_index]
+#         # print("3. I am arr[i] being set to to arr[cur_index]", arr[i])
+
+#         arr[cur_index] = x  # index that had the smallest number being swapped
+#         # The index where the smaller number was is now getting the bigger number
+#         # print("4. ", arr[cur_index])
+
+#     return arr
+
+# Type 2 For Loop Selection Sort
+
+
 def selection_sort(arr):
-    # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
-        # Find minimum element
+
+    for i in range(0, len(arr)-1):
         cur_index = i
-        # print("I am i in 1st loop:", cur_index)
-        # have to not do len(arr) - 1 to access last element
         for j in range(i+1, len(arr)):
-            # print("I am J: ", j)
-            # print("J Loop 1. I am arr[cur_index]", arr[cur_index])
-            # print("J Loop 2. I am arr[j]", arr[j])
             if arr[cur_index] > arr[j]:
-                cur_index = j  # Looping through values in array and setting cur_index to the array number that is the smallest
-                # print("J Loop >. cur_index set to j", cur_index)
+                cur_index = j
 
-        # Swap minimum element
-        # print("I occur after j loop finishes then goes back to i loop -> j loop")
-        # Current number you are on in the i for loop. Setting x to be first array number
-        x = arr[i]
-        # print("1. I am x", x)
-        # print("2. I am arr[cur_index]", arr[cur_index])
-        # arr[cur_index] is the lowest number found and being set to current i position(being near beginning. So smallest going to front of array)
-        # position arr[i] is being set to the lowest number found in j loop. Swap begins
-        arr[i] = arr[cur_index]
-        # print("3. I am arr[i] being set to to arr[cur_index]", arr[i])
-
-        arr[cur_index] = x  # index that had the smallest number being swapped
-        # The index where the smaller number was is now getting the bigger number
-        # print("4. ", arr[cur_index])
+        # swap
+        arr[i], arr[cur_index] = arr[cur_index], arr[i]
 
     return arr
 
 
-selection_sort([5, 3, 4, 2, 1])
+print(selection_sort([5, 3, 4, 2, 1]))
 
 
 # TO-DO:  implement the Bubble Sort function below
@@ -53,7 +72,7 @@ def bubble_sort(arr):
     return arr
 
 
-bubble_sort([5, 3, 4, 2, 1])
+# bubble_sort([5, 3, 4, 2, 1])
 # Ex after first I loop (0):
 # [3,4,2,1,5]
 # I loop(1):
--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,4 +1,8 @@
 # TO-DO: complete the helper function below to merge 2 sorted arrays
+
+# Ex 1
+
+
 def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
@@ -12,27 +16,109 @@ def merge(arrA, arrB):
 # merge([2, 2, 3], [2, 2, 3])
 
 
-# TO-DO: implement the Merge Sort function below USING RECURSION
+# # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort(arr):
-    # TO-DO
+    #     # TO-DO
     if len(arr) > 1:
         middle = len(arr) // 2
-        left = arr[:middle]
-        right = arr[middle:]
+        left_half = arr[:middle]
+        right_half = arr[middle:]
+        print("1. Left: ", left_half)
+        print("1. Right: ", right_half)
+        print("Array: ", arr, "\n")
 
         # Recursive call on each half to divide them
-        merge_sort(left)
-        merge_sort(right)
-        # print("I am Middle: ", middle)
-        # print("I am the Left: ", left)
-        # print("I am the right: ", right)
-        # print("Printing mergeSort left: ", merge_sort(left))
-        # print("Printing mergeSort Right: ", merge_sort(right))
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        # iterator to traverse left_half
+        i = 0
+        # iterator traverse right_half
+        j = 0
+        # Iterator of main
+        k = 0
+
+        while i < len(left_half) and j < len(right_half):
+            print(left_half)
+            print(right_half)
+            print("\nMain While")
+            print("Array: ", arr)
+            print("left_half[i]: ", left_half[i])
+            print("right_half[j]: ", right_half[j], "\n")
+            if left_half[i] < right_half[j]:
+                print(f'{left_half[i]} < {right_half[j]}')
+                print("MW IfLeft left_half[i] :", left_half[i])
+                arr[k] = left_half[i]
+                print("MW IfLeft arr[k]: ", arr[k])
+                print("MW IfLeft arr: ", arr, "\n")
+
+                i += 1
+            else:
+                print(f'{left_half[i]} > {right_half[j]}')
+                print("MW IfRight right_half[j] :", right_half[j])
+                arr[k] = right_half[j]
+                print("MW IfRight arr[k]: ", arr[k])
+                print("MW IfRight arr: ", arr, "\n")
+                j += 1
+
+            k += 1
+
+        # For remaining values
+        while i < len(left_half):
+            print("While Left.")
+            print("WL left_half[i]: ", left_half[i])
+            arr[k] = left_half[i]
+            print("WL : ", arr)
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            print("While Right")
+            print("WR right_half[j]: ", right_half[j])
+            arr[k] = right_half[j]
+            print("WR : ", arr)
+            j += 1
+            k += 1
 
     return arr
 
 
-merge_sort([3, 7, 1, 5, 4])
+print(merge_sort([3, 7, 1, 5, 11, 4, 2]))
+
+
+# Ex 2
+
+# def merge(leftArray, rightArray):
+#     # merged_arr = [0] * elements
+#     merged_arr = []
+
+#     i, j = 0, 0
+
+#     while i < len(leftArray) and j < len(rightArray):
+#         if leftArray[i] <= rightArray[j]:
+#             merged_arr.append(leftArray[i])
+#             i += 1
+#         else:
+#             merged_arr.append(rightArray[j])
+#             j += 1
+
+#     merged_arr += leftArray[i:]
+#     merged_arr += rightArray[j:]
+
+#     return merged_arr
+
+
+# def merge_sort(arr):
+#     if len(arr) < 2:
+#         return arr
+#     middle = len(arr) // 2
+#     left_half = arr[:middle]
+#     right_half = arr[middle:]
+
+#     left_half_merge_sort = merge_sort(left_half)
+#     right_half_merge_sort = merge_sort(right_half)
+
+#     return merge(left_half_merge_sort, right_half_merge_sort)
 
 
 # STRETCH: implement an in-place merge sort algorithm
