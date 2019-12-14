cbf06e53fdac577ea6bbb18a47528b265345f4d0
Alberto-Funk <laptopreneur@gmail.com>
2019-12-14 05:45:53 -0800

    selection sort and bubble sort complete

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,7 +1,21 @@
-# TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
+# TO-DO: Complete the selection_sort() function below
+def selection_sort(arr):
+    for count, _ in enumerate(arr):
+        smallest = arr[count]
+        smallestIndex = count
+        inner_count = count + 1
+        while inner_count < len(arr):
+            if smallest > arr[inner_count]:
+                smallest = arr[inner_count]
+                smallestIndex = inner_count
+            inner_count += 1
+        if smallestIndex != count:
+            arr[smallestIndex] = arr[count]
+            arr[count] = smallest
     return arr
-print(selection_sort([8, 250, 8000, 135, 6584, 23, 7, 63, 8, 3, 4, 3, 2, 6, 5, 20, 50, 8, 3, 9, 4]))
+
+
+#print(selection_sort([8, 250, 8000, 135, 6584, 23, 7, 63, 8, 3, 4, 3, 2, 6, 5, 20, 50, 8, 3, 9, 4]))
 
 
 """
@@ -32,11 +46,18 @@ console.log(SelectionSort([8, 250, 8000, 135, 6584, 23, 7, 1.6, 8, 1.8, 2, 20.8,
 """
 
 
-
 # TO-DO:  implement the Bubble Sort function below
-def bubble_sort( arr ):
+def bubble_sort(arr):
+    for _ in arr:
+        for inner_count, _ in enumerate(range(len(arr) - 1)):
+            if arr[inner_count + 1] < arr[inner_count]:
+                temp = arr[inner_count + 1]
+                arr[inner_count + 1] = arr[inner_count]
+                arr[inner_count] = temp
     return arr
-print(bubble_sort([8, 250, 8000, 135, 6584, 23, 7, 63, 8, 3, 4, 3, 2, 6, 5, 20, 50, 8, 3, 9, 4]))
+
+#print(bubble_sort([8, 250, 8000, 135, 6584, 23, 7, 63, 8, 3, 4, 3, 2, 6, 5, 20, 50, 8, 3, 9, 4]))
+
 
 """
 function bubbleSort(list) {
@@ -58,11 +79,7 @@ console.log(bubbleSort([8, 250, 8000, 135, 6584, 23, 7, 1.6, 8, 1.8, 2, 20.8, 6,
 """
 
 
-
-
-
-
 # STRETCH: implement the Count Sort function below
-def count_sort( arr, maximum=-1 ):
+def count_sort(arr, maximum=-1):
 
     return arr
