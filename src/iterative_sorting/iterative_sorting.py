8eed3008d9354dd83b5c36bd878d439e9e95ec3e
Michael Redig <mredig@gmail.com>
2020-01-20 02:27:23 -0600

    some pep8 autoformatting i think

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,17 +1,20 @@
-# TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
+# TO-DO: Complete the selection_sort() function below
+def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
         # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
+        # (hint, can do in 3 loc)
 
+        # TO-DO: swap
+
+    return arr
 
 
         # TO-DO: swap
 
+# TO-DO:  implement the Bubble Sort function below
 
 
 def bubble_sort(arr):
@@ -27,13 +30,12 @@ def bubble_sort(arr):
     return arr
 
 
-# TO-DO:  implement the Bubble Sort function below
-def bubble_sort( arr ):
-
-    return arr
-
+bubble_sort(arr1)
+print(arr1)
 
 # STRETCH: implement the Count Sort function below
-def count_sort( arr, maximum=-1 ):
+
+
+def count_sort(arr, maximum=-1):
 
     return arr
