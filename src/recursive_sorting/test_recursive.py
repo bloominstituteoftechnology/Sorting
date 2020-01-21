2db25f34cebf092e9a27831485f14973b2bf1892
Michael Redig <mredig@gmail.com>
2020-01-21 13:08:50 -0600

    updated with autoformatting

--- src/recursive_sorting/test_recursive.py
+++ src/recursive_sorting/test_recursive.py
@@ -2,6 +2,7 @@ import unittest
 import random
 from recursive_sorting import *
 
+
 class RecursiveSortingTests(unittest.TestCase):
     def test_merge_sort(self):
         arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
@@ -10,10 +11,10 @@ class RecursiveSortingTests(unittest.TestCase):
         arr4 = [0, 1, 2, 3, 4, 5]
         arr5 = random.sample(range(200), 50)
 
-        self.assertEqual(merge_sort(arr1), [0,1,2,3,4,5,6,7,8,9])
+        self.assertEqual(merge_sort(arr1), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
         self.assertEqual(merge_sort(arr2), [])
         self.assertEqual(merge_sort(arr3), [2])
-        self.assertEqual(merge_sort(arr4), [0,1,2,3,4,5])
+        self.assertEqual(merge_sort(arr4), [0, 1, 2, 3, 4, 5])
         self.assertEqual(merge_sort(arr5), sorted(arr5))
 
     # Uncomment this test to test your in-place merge sort implementation
