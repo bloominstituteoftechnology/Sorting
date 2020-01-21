dadc3adcfc8e2feb05326cf582077df676d7dd86
SamH3pn3r <samuelhepner03@gmail.com>
2020-01-21 13:23:23 -0800

    'day1'

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -5,26 +5,25 @@ def selection_sort( arr ):
         cur_index = i
         smallest_index = cur_index
         # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
-
-
-
+        # (hint, can do in 3 lines)
+        for j in range(i+1, len(arr)):
+            if (arr[smallest_index] > arr[j]):
+                smallest_index = j
         # TO-DO: swap
-
-
-
-
+        arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
-
+    for i in range(len(arr)):
+        for j in range(0, len(arr)-i-1):
+            if (arr[j] > arr[j+1]):
+                arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
 
 
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
-
+    
     return arr
--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,16 +1,23 @@
-# TO-DO: complete the helpe function below to merge 2 sorted arrays
+# TO-DO: complete the helper function below to merge 2 sorted arrays
 def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
-    # TO-DO
-    
+    for i in range(len(arrA)):
+        merged_arr[i] = arrA[i]
+    for j in range(len(arrA), elements):
+        merged_arr[j] = arrB[j-len(arrA)]
     return merged_arr
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
+    arrA = arr[:len(arr)//2]
+    arrB = arr[len(arr)//2:]
 
+    
+    
+    arr = merge(arrA, arrB)
     return arr
 
 
@@ -31,3 +38,7 @@ def merge_sort_in_place(arr, l, r):
 def timsort( arr ):
 
     return arr
+
+
+arr = [1, 2, 3, 4, 5]
+print(merge_sort(arr))
