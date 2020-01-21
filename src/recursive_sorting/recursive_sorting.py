e4e9d07d68784d666df4213b4f95f9c7cbd27aac
Michael Redig <mredig@gmail.com>
2020-01-21 13:58:41 -0600

    added quicksort

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,3 +1,32 @@
+def quicksort(arr):
+    return quicksortHelper(arr, 0, len(arr) - 1)
+
+
+def quicksortHelper(arr, low, high):
+    pivotIndex = high
+
+    if low >= high:
+        return
+
+    for compareIndex in range(low, high):
+        value = arr[compareIndex]
+        pivot = arr[pivotIndex]
+        if value > pivot:
+            arr[compareIndex], arr[pivotIndex] = arr[pivotIndex], arr[compareIndex]
+            pivotIndex, compareIndex = compareIndex, pivotIndex
+            newPivotIndex = compareIndex - 1
+            arr[pivotIndex], arr[newPivotIndex] = arr[newPivotIndex], arr[pivotIndex]
+            pivotIndex = newPivotIndex
+
+    quicksortHelper(arr, 0, pivotIndex - 1)
+    quicksortHelper(arr, pivotIndex, high)
+
+
+arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+quicksort(arr1)
+print(arr1)
+
+
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
 def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
