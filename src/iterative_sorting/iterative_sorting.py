cf82a8fa6b68267d2300f06759c996706bbf563e
jkaunert <44586402+jkaunert@users.noreply.github.com>
2020-01-22 19:47:13 -0600

    (feat) part 1 done

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,30 +1,32 @@
-# TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
+# TO-DO: Complete the selection_sort() function below
+def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
         # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
-
-
+        # (hint, can do in 3 loc)
+        for index in range(cur_index, len(arr)):
+            if arr[index] < arr[smallest_index]:
+                smallest_index = index
 
         # TO-DO: swap
-
-
-
+        arr[smallest_index], arr[cur_index] = arr[cur_index], arr[smallest_index]
 
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
-def bubble_sort( arr ):
-
+def bubble_sort(arr):
+    for num in range(len(arr) - 1, 0, -1):
+        for index in range(num):
+            if arr[index] > arr[index + 1]:
+                swap = arr[index]
+                arr[index] = arr[index + 1]
+                arr[index + 1] = swap
     return arr
 
 
-# STRETCH: implement the Count Sort function below
-def count_sort( arr, maximum=-1 ):
-
-    return arr
+# # STRETCH: implement the Count Sort function below
+# def count_sort(arr, maximum=-1):
+#     return arr
--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -15,19 +15,19 @@ def merge_sort( arr ):
 
 
 # STRETCH: implement an in-place merge sort algorithm
-def merge_in_place(arr, start, mid, end):
-    # TO-DO
-
-    return arr
-
-def merge_sort_in_place(arr, l, r): 
-    # TO-DO
-
-    return arr
-
-
-# STRETCH: implement the Timsort function below
-# hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
-def timsort( arr ):
-
-    return arr
+# def merge_in_place(arr, start, mid, end):
+#     # TO-DO
+#
+#     return arr
+#
+# def merge_sort_in_place(arr, l, r):
+#     # TO-DO
+#
+#     return arr
+#
+#
+# # STRETCH: implement the Timsort function below
+# # hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
+# def timsort( arr ):
+#
+#     return arr
