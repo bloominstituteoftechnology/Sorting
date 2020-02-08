bdc153d2b5163d7d3ba176d9dc9d3bae022d523e
ElijahMcKay <elijahmckay10@gmail.com>
2020-02-07 19:06:29 -0600

    finished insertion_sort.py

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -13,10 +13,25 @@ def selection_sort( arr ):
         # TO-DO: swap
 
 
-
-
     return arr
 
+testArr = [3, 6, 2, 7, 100, 21, 401, 1, 22, 4, 7, 5]
+
+def insertion_sort(testArr):
+    # capturing the entire range minus the first variable.  The first variable has nothing to the left to compare itself to, so its not necessary
+    length_of_index = range(1, len(testArr))
+
+    for i in length_of_index:
+        # why does this need a variable? 
+        value_to_sort = testArr[i]
+        # why can't I just use testArr[i] instead of value_to_sort?
+        while testArr[i - 1] > value_to_sort and i > 0:
+            # doing a swap if the above conditions are true, this is just easy python syntax for a swap
+            testArr[i], testArr[i - 1] = testArr[i - 1], testArr[i]
+            # decrementing i, or it will be an infinite loop
+            i = i - 1
+
+    return testArr
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
@@ -28,3 +43,6 @@ def bubble_sort( arr ):
 def count_sort( arr, maximum=-1 ):
 
     return arr
+
+
+print(insertion_sort(testArr))
