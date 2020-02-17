224a0d8460e9363fcdda18e97acf41c7607fe42e
ElijahMcKay <elijahmckay10@gmail.com>
2020-02-17 13:33:53 -0600

    redid insertion sort, moving on to bubble

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -2,56 +2,47 @@
 def selection_sort( arr ):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
+        # cur_index = i
+        smallest_index = i
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
+        for j in range(i + 1, len(arr)):
+            if arr[smallest_index] > arr[j]:
+                # we're just constantly updating the smallest value with indexes that are smaller until we've looped through everything
+                smallest_index = j
 
-
-
+        # we don't do the swapping until we have looped through the entire list!  
         # TO-DO: swap
+        arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
 
 
     return arr
 
-testArr = [3, 6, 2, 7, 100, 21, 401, 1, 22, 4, 7, 5]
+testArr = [5, 2, 6, 3, 8, 10, 13, 3, 4, 6, 9, 100, 2, 0, 1]
 
-def insertion_sort(testArr):
-    # capturing the entire range minus the first variable.  The first variable has nothing to the left to compare itself to, so its not necessary
-    length_of_index = range(1, len(testArr))
+def insertion_sort(arr):
+    # Step 1: loop through the array, the first index in the array is going to be our "sorted" bit
+    # don't need minus 1 because range is (inc, exclusive)
+    for i in range(1, len(arr)):
+        print('i', i)
+    # Step 2: Take the indexes (except the last one) after the current array and move them to the left until they are at the appropriate position in the sorted part
+        while arr[i] < arr[i - 1] and i > 0:
+            print('swap', i)
+            temp = arr[i]
+            arr[i] = arr[i - 1]
+            arr[i - 1] = temp
 
-    for i in length_of_index:
-        # why does this need a variable? 
-        value_to_sort = testArr[i]
-        # why can't I just use testArr[i] instead of value_to_sort?
-        while testArr[i - 1] > value_to_sort and i > 0:
-            # doing a swap if the above conditions are true, this is just easy python syntax for a swap
-            testArr[i], testArr[i - 1] = testArr[i - 1], testArr[i]
-            # decrementing i, or it will be an infinite loop
             i = i - 1
 
-    return testArr
+    # Step 3:
+
+    return arr
+
+
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
-    # Step 1: loop through the array
-    # get a variable for the length of the array
-    length = len(arr)
-    for i in range(length):
-        # setting a variable so we can tell if anything has been swapped or not
-        swapped = False
-        # adding - i - 1 to the range because we don't have to compare the last item against anything
-        # why do we need the -i part? Why not just - 1 to stand for the length minus 1?
-        for j in range(0, length - i - 1):
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
-                # keeping swapped as true as long as a change was made
-                swapped = True
 
-        if swapped == False:
-            # breaking the loop if we don't make a change
-            break
-    
     return arr
 
 # STRETCH: implement the Count Sort function below
@@ -60,5 +51,9 @@ def count_sort( arr, maximum=-1 ):
     return arr
 
 
-# print(insertion_sort(testArr))
-print(bubble_sort(testArr))
+print(insertion_sort(testArr))
+# print(bubble_sort(testArr))
+# print(selection_sort(testArr))
+
+
+
