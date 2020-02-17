3741397a7007784b632fea00db726117a1e408c8
Gatrrr <davidgatrmacaulay@gmail.com>
2020-02-17 14:22:49 -0800

    day 1 MVP

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -5,12 +5,16 @@ def selection_sort( arr ):
         cur_index = i
         smallest_index = cur_index
         # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
+        # (hint, can do in 3 loc)
+        for j in range(smallest_index +1, len(arr)):
+            if arr[smallest_index] > arr[j]:
+                smallest_index = j
              
 
 
 
         # TO-DO: swap
+        arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
 
 
 
@@ -20,6 +24,13 @@ def selection_sort( arr ):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
+    for i in range(len(arr)-1, 0,-1):
+        for j in range(i):
+            if arr[j] > arr[j+1]:
+                temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = temp
+
 
     return arr
 
