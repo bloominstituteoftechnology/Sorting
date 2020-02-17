3481bea402dc497c32cfb2b2bc21d542d600bad8
heiligbasil <basilief@yahoo.com>
2020-02-17 15:51:25 -0800

    Build the Selection sort function

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,5 +1,3 @@
-
-
 # Insertion Algorithm
 def insertion_sort(arr):
     # 1. Separate the first element from the rest of the array. Think about it as a sorted list of one element.
@@ -19,19 +17,29 @@ def insertion_sort(arr):
     return arr
 
 
+# Algorithm
+# 1. Start with current index = 0
+# 2. For all indices EXCEPT the last index:
+#     a. Loop through elements on right-hand-side of current index and find the smallest element
+#     b. Swap the element at current index with the smallest element found in above loop
 def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-
-        # TO-DO: swap
-
+        smallest_index = i
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
+        copied_item = arr[i]
+        arr[i] = arr[smallest_index]
+        arr[smallest_index] = copied_item
     return arr
 
 
+# Algorithm
+# 1. Loop through your array
+#     Compare each element to its neighbor
+#     If elements in wrong position (relative to each other, swap them)
+# 2. If no swaps performed, stop. Else, go back to the element at index 0 and repeat step 1.
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     return arr
