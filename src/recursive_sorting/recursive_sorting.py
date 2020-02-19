297e3604c6d8e54a5d9910bfc56b1979202ed89d
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-02-19 13:55:57 +0100

    complete merge function

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -10,12 +10,13 @@ def merge( arrA, arrB ):
     while a < len(arrA) and b < len(arrB):
         if arrA[a] < arrB[b]:
             merged_arr[c] = arrA[a]
-            c = c + 1
-            a = a + 1
+            a =  a + 1
+            c = c + 1      
         else:
             merged_arr[c] = arrB[b]
-            c = c + 1
-            b = b + 1
+            b =  b + 1
+            c = c + 1       
+
     while a < len(arrA):
         merged_arr[c] = arrA[a]
         c = c + 1
@@ -25,17 +26,42 @@ def merge( arrA, arrB ):
         c = c + 1
         b = b + 1
     
-    print(merged_arr)
-
+    # print(merged_arr)
     return merged_arr
 
-merge([1, 6, 5], [4, 8, 3])
+# merge([1, 3, 5, 7], [2, 4, 6, 8])
 # TO-DO: implement the Merge Sort function below USING RECURSION
-def merge_sort( arr ):
-    # TO-DO
 
-    return arr
+# import math
+# def merge_sort( arr ):
+#     # TO-DO
+#     print(arr)
+#     if len(arr) < 2:
+#         return arr
+    
+#     half_point = math.ceil(len(arr) / 2)
+#     # size = len(arr)
 
+#     h1 = [0] * half_point
+#     h2 = []
+
+#     for i in range(0, len(arr)):
+#         if (i < half_point):
+#             h1[i] = arr[i]
+#         else:
+#             h2.append(arr[i])
+    
+#     # for k in range(half_point, len(arr)):
+#     #     h2[k] = arr[k]
+    
+#     h1 = merge_sort(h1)
+#     h2 = merge_sort(h2)
+
+#     arr = merge(h1, h2)
+
+#     return arr
+
+# merge_sort([1, 5, 8, 4, 2, 9, 6, 0, 3, 7])
 
 # STRETCH: implement an in-place merge sort algorithm
 def merge_in_place(arr, start, mid, end):
