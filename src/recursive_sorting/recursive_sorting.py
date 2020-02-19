3e8f689c9eb6ced6d242ed1e8ff9d0504c25a7f7
micah riley <micahriley88@gmail.com>
2020-02-19 12:09:15 -0500

    Completed Day 2's MVP

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -3,16 +3,38 @@ def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
     # TO-DO
-    
+    a = 0
+    b = 0
+
+    for i in range(0, elements):
+        if a >= len(arrA):
+            merged_arr[i] = arrB[b]
+            b += 1
+        elif b >= len(arrB):
+            merged_arr[i] = arrA[a]
+            a += 1
+        elif arrA[a] < arrB[b]:
+            merged_arr[i] = arrA[a]
+            a += 1
+        else:
+            merged_arr[i] = arrB[b]
+            b += 1
     return merged_arr
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
+    a = len(arr)
 
-    return arr
+    if a < 2:
+        return arr
+    middle = a // 2
+    array_1 = arr[:middle]
+    array_2 = arr[middle:]
 
+
+    return merge(merge_sort(array_1), merge_sort(array_2))
 
 # STRETCH: implement an in-place merge sort algorithm
 def merge_in_place(arr, start, mid, end):
