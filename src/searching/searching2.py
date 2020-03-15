7081bfcce29edae61c16163afeea94b9528dd9f3
oops <oops@localhost.localdomain>
2020-03-15 18:50:34 -0400

    starting branch

--- src/searching/searching2.py
+++ src/searching/searching2.py
@@ -1,38 +1,3 @@
-# STRETCH: implement Linear Search
-def linear_search(arr, target):
-
-    # TO-DO: add missing code
-
-    return -1  # not found
-
-
-# STRETCH: write an iterative implementation of Binary Search
-def binary_search(arr, target):
-
-    if len(arr) == 0:
-        return -1  # array empty
-
-    low = 0
-    high = len(arr) - 1
-
-    # TO-DO: add missing code
-
-    return -1  # not found
-
-
-# STRETCH: write a recursive implementation of Binary Search
-def binary_search_recursive(arr, target, low, high):
-
-    middle = (low + high) // 2
-
-    if len(arr) == 0:
-        return -1  # array empty
-    # TO-DO: add missing if/else statements, recursive calls
-
-
-###
-
-
 # Title: binary search experiment 2020.03.15 v2 11:41ET GGA
 # note...the number division system here probably runs into too many edge cases
 # is better to change frame of reference to the region of search
