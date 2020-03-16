01a5c6869e296483e710acf4b238ca853c9981b6
chip <chiposaur@gmail.com>
2020-03-16 16:46:10 -0500

    selection sort completed

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,28 +1,51 @@
-# TO-DO: Complete the selection_sort() function below 
+arr = [3,22,17,1,20,8,31,137, 12, 4, 19, 20, 2, 9]
+# arr = [7, 12, 4, 19, 20, 2, 9]
+# arr = [3,22,17,1,20,8,31,13]
+print(arr,"Begin selection \n")
+
+# TO-DO: Complete the selection_sort() function below
+
+
 def selection_sort( arr ):
+	length = len(arr)
+	breakcount =0
 	# loop through n-1 elements
-	for i in range(0, len(arr) - 1):
+	for i in range(0, len(arr) - 2):
+		breakcount+=1
+		
 		cur_index = i
 		smallest_index = cur_index
-		# TO-DO: find next smallest element
-		# (hint, can do in 3 loc) 
-			 
 
+		for j in range( i + 1, length ):
+			if arr[j] < arr[smallest_index]:
+				smallest_index = j
+		
+		if smallest_index != i:
+			move = arr.pop(smallest_index)
+			arr.insert(i, move)
+		
+		if breakcount > 20:
+			print(20, 20)
+			break
 
-
-		# TO-DO: swap
-
-
-
+		print(29,arr)
 
 	return arr
+
+print(35,selection_sort(arr),"selection\n\n") 
+
+
+
+
 
 
 # TO-DO:  implement the Bubble Sort function below
 
-arr = [7, 12, 4, 19, 20, 2, 9]
-print(24,arr,"Begin\n")
+arr = [3,22,17,1,20,8,31,137, 12, 4, 19, 20, 2, 9]
+# arr = [7, 12, 4, 19, 20, 2, 9]
+# arr = [3,22,17,1,20,8,31,13]
 
+print(arr,"Begin bubble\n")
 
 def bubble_sort( arr ):
 
@@ -46,16 +69,15 @@ def bubble_sort( arr ):
 		if not working:
 			break
 
-		if breakcount > 10:
-			print(10, 10)
+		if breakcount > 20:
+			print(20, 20)
 			break
 
 		print(65,arr)
 		
 	return arr
 
-
-print("\nbubble", bubble_sort(arr)) 
+print(77,bubble_sort(arr),"bubble") 
 
 
 # STRETCH: implement the Count Sort function below
