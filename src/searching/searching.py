d90350f1a28ce00cdd64857f47494318a83fb424
mefrem <maxim.efremov@gmail.com>
2020-03-16 19:51:56 -0400

    First day's work on algorithms

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,23 +1,35 @@
-# STRETCH: implement Linear Search				
+# STRETCH: implement Linear Search
 def linear_search(arr, target):
-  
-  # TO-DO: add missing code
-
-   return -1   # not found
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
+    else:
+        return -1
 
 
 # STRETCH: write an iterative implementation of Binary Search 
-def binary_search(arr, target):
+def find_value_binary(arr, value):
+    if len(arr) <= 1:
+        # TODO Handle edge
+        pass
 
-  if len(arr) == 0:
-    return -1 # array empty
-    
-  low = 0
-  high = len(arr)-1
+    first = 0
+    last = (len(arr) - 1)
+    found = False
 
-  # TO-DO: add missing code
-
-  return -1 # not found
+    while first <= last and not found:
+        # find middle using integer divsion
+        middle = (first + last) // 2
+        if arr[middle] == value:
+            found = True
+        else:
+            if value < arr[middle]:
+                # search lower half of remainder
+                last = middle - 1
+            else:
+                # search upper half of remainder
+                first = middle + 1
+    return found
 
 
 # STRETCH: write a recursive implementation of Binary Search 
