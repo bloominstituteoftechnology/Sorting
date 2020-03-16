8b4a8c5d3e7092970b764a3c45d5f316099b4fe4
Albert Yakubov <46692454+albert-yakubov@users.noreply.github.com>
2020-03-16 13:51:05 -0600

    Revert "Update iterative_sorting.py"
    This reverts commit 2f28eb3f626d3fb7437b58c2ee630465fa78c89c.

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -6,32 +6,21 @@ def selection_sort( arr ):
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-        # find smallest element
-        for j in range(cur_index, len(arr)):
-            if(arr[smallest_index] > arr[j]):
-                smallest_index = j
-        if(arr[cur_index] > arr[smallest_index]):
-            low = arr[smallest_index]
-            high = arr[cur_index]
-            #now do the swith or moving around in given array
-            arr[cur_index] = low
-            arr[smallest_index] = high        
-        
+             
+
+
+
+        # TO-DO: swap
+
+
+
+
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
-    n = len(arr)
 
-    # go through element of the array
-    for i in range(n):
-        #last element of the array 
-        for j in range(0, n-i-1):
-            # going through bubble sort go through elements again and keep
-            # switching until elements allign
-            if arr[j] > arr[j+1] : 
-                arr[j], arr[j+1] = arr[j+1], arr[j] 
     return arr
 
 
