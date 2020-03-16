ad3086da3be3b6ff3b88af81194b2b947f46854e
mefrem <maxim.efremov@gmail.com>
2020-03-16 19:53:32 -0400

    Partially completed stretch goals

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -7,6 +7,31 @@ def linear_search(arr, target):
         return -1
 
 
+# STRETCH: write an iterative implementation of Binary Search 
+def find_value_binary(arr, value):
+    if len(arr) <= 1:
+        # TODO Handle edge
+        pass
+
+    first = 0
+    last = (len(arr) - 1)
+    found = False
+
+    while first <= last and not found:
+        # find middle using integer divsion
+        middle = (first + last) // 2
+        if arr[middle] == value:
+            found = True
+        else:
+            if value < arr[middle]:
+                # search lower half of remainder
+                last = middle - 1
+            else:
+                # search upper half of remainder
+                first = middle + 1
+    return found
+
+
 # STRETCH: write a recursive implementation of Binary Search 
 def binary_search_recursive(arr, target, low, high):
   
