ba32342c31777b85e93ef4b6598b3937c53694db
Anthony Hill <hillan1152@gmail.com>
2020-03-17 09:50:58 -0400

    MVP

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -17,12 +17,36 @@ def selection_sort(arr):
 # TO-DO:  implement the Bubble Sort function below
 
 
+# def bubble_sort(arr):
+#     swap = False
+#     if swap == True:
+#         arr[cur_index], arr[next_index] = arr[next_index], arr[cur_index]
+#     for i in range(0, len(arr)-1):
+#         cur_index = i
+#         next_index = cur_index
+#         # Create loop throughout next number to compare
+#         for j in range(cur_index, len(arr)):
+#             if arr[j] > arr[next_index]:
+#                 next_index = j
+#                 arr[cur_index], arr[next_index] = arr[next_index], arr[cur_index]
+#         # if curr index is greater than next index, swap
+#         # then add 1 to current index
+
+#         # else if it's not, add 1 to current index and continue to next step
+#     return arr
+
 def bubble_sort(arr):
-
+    swap = True
+    while swap:
+        swap = False
+        for i in range(0, len(arr) - 1):
+            if arr[i] > arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                swap = True
     return arr
-
-
 # STRETCH: implement the Count Sort function below
+
+
 def count_sort(arr, maximum=-1):
 
     return arr
