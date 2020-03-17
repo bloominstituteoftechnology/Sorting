080a408bd1f5907a0aeee1e6dfad6a2ce2241472
wjonke <wesjonke@gmail.com>
2020-03-17 18:19:50 -0400

    added merge function to recursive lesson and working on merge sort

---
+++ src/recursive_sorting/notes.py
@@ -0,0 +1,123 @@
+'''
+def my_recursion(n):
+    print(n)
+    if n == 100:
+        return
+​
+    my_recursion(n+1)
+    my_recursion(n+1)
+​
+​
+my_recursion(1)
+​
+​
+                                                      Fibonacci
+
+​
+ 0, 1 - Base - if we solve recursively move towards it
+​
+ 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
+​
+ 10th fib number?
+​
+ 9th fib + 8th fib
+​
+ 9th fib is?
+​
+ 8th plus 7th
+​
+ 8th fib
+​
+ 7th plus sixth
+​
+ base case is....? 1 and/or zero
+​
+print the nth fib number
+def recursive_fib(n):
+     base case
+     test for negatives (TODO)
+     if n is 0
+    if n == 0:
+        return 0
+     return 0
+     if n is 1
+    if n == 1:
+        return 1
+     return 1
+​
+    if we're not on the base case
+    return recursion of n-1 + n-2
+    n_minus_1 = recursive_fib(n-1)
+    n_minus_2 = recursive_fib(n-2)
+    return n_minus_1 + n_minus_2
+​
+print(recursive_fib(3))
+​
+​/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+                                            quick sort
+
+Quick Sort
+How It Works
+Another divide and conquer algorithm, Quick Sort requires us to…
+
+Choose a pivot
+Move everything smaller than the pivot to one side
+Move everything larger than the pivot to the other side
+Repeat steps 1-3 on the left-hand side and right-hand side of the partition
+
+
+[ 5  9  3  7  2  8  1  6]
+​
+pick first number
+​
+append all smaller to new array left
+append all bigger to new array right
+​
+[3 2 1]  [5]  [9 7 8 6]
+​
+recurse left
+​
+[2 1 ][3][]     [5]  [9 7 8 6]
+​
+recurse left
+​
+[1][2][][3][]     [5]  [9 7 8 6]
+​
+recurse left
+   1 is sorted,  base case
+​
+recurse right
+    empty array base case
+​
+recurse right
+    empty array, base case
+​
+#recurse right
+[1][2][][3][]     [5]  [9 7 8 6]
+​
+[1][2][][3][]     [5] [7 8 6][9][]
+​
+#recurse left
+​
+[1][2][][3][]     [5] [6][7][8][  ][9][]
+​
+[1, 2] [3]
+[1, 2,  3]
+​
+plan
+base case:  if array length 0 or 1
+ return array
+else:
+pick pivot might as well pick first because its unsorted, none are better
+put anything smaller into left array
+put anything bigger into right array
+return quicksort(left) + quicksort(right)
+
+Merge                                               Merge Sort
+How It Works
+This is a “divide and conquer” algorithm.
+First, the original collection must be divided in half
+until we have broken the entire thing down to single collections
+each with a length of 1.
+'''
--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,33 +1,74 @@
-# TO-DO: complete the helpe function below to merge 2 sorted arrays
-def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
-    merged_arr = [0] * elements
+# TO-DO: complete the helper function below to merge 2 sorted arrays
+
+arrA = [1, 2, 3, 4, 5]
+arrB = [6, 7, 8, 9, 10]
+
+print('\n~~~ merge function starts ~~~~')
+def merge(arrA, arrB):
+    # elements = len(arrA) + len(arrB)
+    # merged_arr = [0] * elements
+    merged_arr = arrA + arrB
     # TO-DO
-    
+
+    print("arrA ~~~>", arrA)
+    print("arrB ~~~>", arrB)
+    print("merged_arr ~~~>", merged_arr, '\n~~~ merge function ends ~~~\n')
+
     return merged_arr
+
+merge(arrA, arrB)
+
+
+
+
+'''
+plan
+base case:  if array length 0 or 1
+ return array
+else:
+pick pivot might as well pick first because its unsorted, none are better
+put anything smaller into left array
+put anything bigger into right array
+return quicksort(left) + quicksort(right)
+'''
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
-def merge_sort( arr ):
-    # TO-DO
+arr = [5,4,6,7,9,1,2,3]
+def merge_sort(arr):
+    set_a= [0, len(arr) //2 ]
+    set_b= [len(arr ) // 2 + 1, arr[-1]]
+    if len(arr) == 1:
+        return arr
 
+    for i in arr:
+        print("arr in range_a = ~~~>",arr[range_a])
+    '\n'
+
+    print("range_a = ~~~>", set_a)
+    print("range_b = ~~~>", set_b)
     return arr
 
+merge_sort(arr)
+print("baseCase or modified arr ~~~>",arr, '\n')
 
-# STRETCH: implement an in-place merge sort algorithm
-def merge_in_place(arr, start, mid, end):
-    # TO-DO
 
-    return arr
 
-def merge_sort_in_place(arr, l, r): 
-    # TO-DO
 
-    return arr
 
+
+
+
+
+# # STRETCH: implement an in-place merge sort algorithm
+# def merge_in_place(arr, start, mid, end):
+#     # TO-DO
+#     return arr
+# def merge_sort_in_place(arr, l, r):
+#     # TO-DO
+# return arr
 
 # STRETCH: implement the Timsort function below
 # hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
-def timsort( arr ):
-
-    return arr
+# def timsort(arr):
+#     return arr
