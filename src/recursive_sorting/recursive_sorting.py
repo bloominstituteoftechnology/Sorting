ce3f8bdefb046f5097f92806bd7ed986078aecdd
heiligbasil <basilief@yahoo.com>
2020-03-27 18:58:16 -0700

    Cleaned up a bit of whitespace

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -88,13 +88,11 @@ def merge_sort(arr):
 # STRETCH: implement an in-place merge sort algorithm
 def merge_in_place(arr, start, mid, end):
     # TO-DO
-
     return arr
 
 
 def merge_sort_in_place(arr, l, r):
     # TO-DO
-
     return arr
 
 
