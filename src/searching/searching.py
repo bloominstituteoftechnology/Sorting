836a5d9844bcadc446a55313568518950f351e90
Michael <mdlevick@gmail.com>
2020-04-13 17:12:55 -0500

    done with project

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,10 +1,11 @@
 # STRETCH: implement Linear Search				
 def linear_search(arr, target):
   
-  # TO-DO: add missing code
+  for i in range(0, len(arr)):
+    if arr[i] == target:
+      return i
 
-   return -1   # not found
-
+  return -1   # not found
 
 # STRETCH: write an iterative implementation of Binary Search 
 def binary_search(arr, target):
@@ -14,8 +15,16 @@ def binary_search(arr, target):
     
   low = 0
   high = len(arr)-1
+  
+  while low <= high:
+    mid = (low + high) // 2
 
-  # TO-DO: add missing code
+    if target < arr[mid]:
+      high = mid - 1
+    elif target > arr[mid]:
+      low = mid + 1
+    else:
+      return mid
 
   return -1 # not found
 
@@ -27,4 +36,13 @@ def binary_search_recursive(arr, target, low, high):
 
   if len(arr) == 0:
     return -1 # array empty
-  # TO-DO: add missing if/else statements, recursive calls
+  elif low > high:
+    return -1
+  elif arr[middle] == target:
+    return middle
+  else:
+    if target < arr[middle]:
+      high = middle - 1
+    else:
+      low = middle + 1
+    return binary_search_recursive(arr, target, low, high)
