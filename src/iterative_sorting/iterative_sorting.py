e7d48e4bf3b86c3d348ec36dc0b89edbdaa1a3f1
Lexie Jiang <lexiemingjiang@gmail.com>
2020-04-13 12:23:40 -0700

    Initial Commit

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,4 +1,5 @@
 # TO-DO: Complete the selection_sort() function below 
+
 def selection_sort( arr ):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
@@ -19,6 +20,7 @@ def selection_sort( arr ):
 
 
 # TO-DO:  implement the Bubble Sort function below
+
 def bubble_sort( arr ):
 
     return arr
