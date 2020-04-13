d58d8922c8dd1c85508e2aeb153b482dba77b70d
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-04-13 18:54:57 -0400

    comments, minor adjustments

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -16,11 +16,13 @@ def insertion_sort(array, compare=compare_ascending):
     # 4. move all checked items "more" than array[i] to the right and insert array[i] at its new index in the "sorted" portion of the array
     # 5. repeat until there are no more items to sort
 
-    # for each item in _mutating_ array
-    for i in range(1, len(array)):
+    n = len(array)
 
-        item = array[i]
-        j = i  # this index moves
+    # for each pair of items in _mutating_ array...
+    for i in range(1, n):
+
+        item = array[i]  # the item to move
+        j = i  # index where item should move to
 
         # from [j] to the left, swap [j] and [j-1] until correct index for item
         while compare(item, array[j - 1]) < 0 and j > 0:
@@ -42,13 +44,15 @@ def selection_sort(array, compare=compare_ascending):
     # 4. swap the "current minimum" with item _after_ the end of the "sorted" portion of array
     # 5. repeat for the remaining "unsorted" portion of array
 
-    # for each item in _mutating_ array
-    for i in range(0, len(array) - 1):
+    n = len(array)  # size of full array
+
+    # for each pair of items in _mutating_ array...
+    for i in range(0, n - 1):
 
         p = i  # index of this iteration's "minimum"
 
-        # find minumum in "unsorted" portion
-        for j in range(i, len(array)):
+        # find the index of the "minumum" in "unsorted" portion...
+        for j in range(i, n):
             if compare(array[j], array[p]) < 0:
                 p = j
 
@@ -68,8 +72,15 @@ def bubble_sort(array, compare=compare_ascending):
     # -- doing this once will move the "maximum" item to the end of array, which is now "sorted"
     # 3. repeat the process on the "unsorted" array until nothing is left
 
-    for i in range(0, len(array) - 1):
-        for j in range(0, len(array) - 1 - i):
+    n = len(array)  # size of full array
+
+    # for each pair of items in _mutating_ array...
+    for i in range(0, n - 1):
+
+        m = n - i  # size of "sorted" portion
+
+        # sort "unsorted" portion...
+        for j in range(0, m - 1):
             if compare(array[j + 1], array[j]) < 0:
                 # swap
                 item = array[j + 1]
