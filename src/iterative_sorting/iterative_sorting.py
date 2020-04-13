8629ad87c08a3cd77cdbb5e64fa105634c961199
Lexie Jiang <lexiemingjiang@gmail.com>
2020-04-13 12:40:52 -0700

    Commit 1: selection_sort done out of iterative_sort.py, test_ran=OK

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -5,16 +5,23 @@ def selection_sort( arr ):
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
+        # TO-DO: find next smallest element (hint, can do in 3 loc) 
 
+        # First, I need to iterate through the elements that remain in the arr
 
+        for x in range(smallest_index + 1, len(arr)):
+        
+            # Second, I need to check if the element is smaller than the currently smallest
+        
+            if arr[x] < arr[smallest_index]:
+        
+                # Third, if this statement is True, I will need to set the new index to be the smallest
+        
+                smallest_index = x
 
-        # TO-DO: swap
+        # TO-DO: swap the arr
 
-
-
+        arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]
 
     return arr
 
