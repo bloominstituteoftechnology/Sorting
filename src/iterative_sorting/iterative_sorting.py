74f8d026c752fa96aa9612d553beadc555b2dfc0
Anthony Hill <hillan1152@gmail.com>
2020-04-13 16:20:53 -0400

    completed selection sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -6,65 +6,24 @@ def selection_sort(arr):
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
+
+        # progress through array looking for smaller number
         for j in range(cur_index, len(arr)):
-            if arr[j] < arr[smallest_index]:
+            if arr[cur_index] > arr[j]:
                 smallest_index = j
         # TO-DO: swap
-        arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]
+        arr[cur_index] = arr[smallest_index]
+
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
-
-
-# def bubble_sort(arr):
-#     swap = False
-#     if swap == True:
-#         arr[cur_index], arr[next_index] = arr[next_index], arr[cur_index]
-#     for i in range(0, len(arr)-1):
-#         cur_index = i
-#         next_index = cur_index
-#         # Create loop throughout next number to compare
-#         for j in range(cur_index, len(arr)):
-#             if arr[j] > arr[next_index]:
-#                 next_index = j
-#                 arr[cur_index], arr[next_index] = arr[next_index], arr[cur_index]
-#         # if curr index is greater than next index, swap
-#         # then add 1 to current index
-
-#         # else if it's not, add 1 to current index and continue to next step
-#     return arr
-
 def bubble_sort(arr):
-    swap = True
-    while swap:
-        swap = False
-        for i in range(0, len(arr) - 1):
-            if arr[i] > arr[i + 1]:
-                arr[i], arr[i + 1] = arr[i + 1], arr[i]
-                swap = True
+
     return arr
+
+
 # STRETCH: implement the Count Sort function below
-
-# THIS DOESN'T WORK
-
-
 def count_sort(arr, maximum=-1):
-    # count number of times each value appears
-    counts = [0] * (maximum + 1)
-    for num in arr:
-        counts[num] += 1
-
-    # overwrite coutns to hold next index an item to store where the next number goes
-    previous = 0
-    for i, count in enumerate(counts):
-        counts[i] = previous
-        previous += count
-
-    sorted_list = [None] * len(arr)
-
-    for item in arr:
-        sorted_list[counts[item]] = item
-        counts[item] += 1
 
     return arr
