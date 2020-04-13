df8684314052f7fe3bd8afcaa3d4838ca0f82dab
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-04-13 13:19:15 -0400

    test_{iterative,recursive}.py: genericize tests

--- src/iterative_sorting/test_iterative.py
+++ src/iterative_sorting/test_iterative.py
@@ -9,39 +9,41 @@ from iterative_sorting import (
 
 class IterativeSortingTest(unittest.TestCase):
 
-    def test_selection_sort(self):
-        arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
-        arr2 = []
-        arr3 = [0, 1, 2, 3, 4, 5]
-        arr4 = random.sample(range(200), 50)
+    test_arrays = (
+        lambda: [1, 5, 8, 4, 2, 9, 6, 0, 3, 7],
+        lambda: [],
+        lambda: [2],
+        lambda: [-1, 0, 1, 2, 3, 4, 5],
+        lambda: random.sample(range(200), 50),
+    )
 
-        self.assertEqual(selection_sort(arr1), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-        self.assertEqual(selection_sort(arr2), [])
-        self.assertEqual(selection_sort(arr3), [0, 1, 2, 3, 4, 5])
-        self.assertEqual(selection_sort(arr4), sorted(arr4))
+    def test_selection_sort(self):
+
+        for test_array in self.test_arrays:
+
+            array = test_array()
+            self.assertEqual(selection_sort(array), sorted(array))
 
     def test_bubble_sort(self):
-        arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
-        arr2 = []
-        arr3 = [0, 1, 2, 3, 4, 5]
-        arr4 = random.sample(range(200), 50)
 
-        self.assertEqual(bubble_sort(arr1), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-        self.assertEqual(bubble_sort(arr2), [])
-        self.assertEqual(bubble_sort(arr3), [0, 1, 2, 3, 4, 5])
-        self.assertEqual(bubble_sort(arr4), sorted(arr4))
+        for test_array in self.test_arrays:
+
+            array = test_array()
+            self.assertEqual(bubble_sort(array), sorted(array))
 
     # Uncomment this test to test your count_sort implementation
     # def test_counting_sort(self):
-    #     arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
-    #     arr2 = []
-    #     arr3 = [1, 5, -2, 4, 3]
-    #     arr4 = random.sample(range(200), 50)
-
-    #     self.assertEqual(count_sort(arr1), [0,1,2,3,4,5,6,7,8,9])
-    #     self.assertEqual(count_sort(arr2), [])
-    #     self.assertEqual(count_sort(arr3), "Error, negative numbers not allowed in Count Sort")
-    #     self.assertEqual(count_sort(arr4), sorted(arr4))
+    #
+    #     for test_array in self.test_arrays:
+    #
+    #         array = test_array()
+    #
+    #         if any(item < 0 for item in array):
+    #             expected_output = "Error, negative numbers not allowed in Count Sort"
+    #         else:
+    #             expected_output = sorted(array)
+    #
+    #         self.assertEqual(count_sort(array), expected_output)
 
 
 if __name__ == '__main__':
--- src/recursive_sorting/test_recursive.py
+++ src/recursive_sorting/test_recursive.py
@@ -8,32 +8,31 @@ from recursive_sorting import (
 
 class RecursiveSortingTests(unittest.TestCase):
 
+    test_arrays = (
+        lambda: [1, 5, 8, 4, 2, 9, 6, 0, 3, 7],
+        lambda: [],
+        lambda: [2],
+        lambda: [-1, 0, 1, 2, 3, 4, 5],
+        lambda: random.sample(range(200), 50),
+    )
+
     def test_merge_sort(self):
-        arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
-        arr2 = []
-        arr3 = [2]
-        arr4 = [0, 1, 2, 3, 4, 5]
-        arr5 = random.sample(range(200), 50)
 
-        self.assertEqual(merge_sort(arr1), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-        self.assertEqual(merge_sort(arr2), [])
-        self.assertEqual(merge_sort(arr3), [2])
-        self.assertEqual(merge_sort(arr4), [0, 1, 2, 3, 4, 5])
-        self.assertEqual(merge_sort(arr5), sorted(arr5))
+        for test_array in self.test_arrays:
 
-    # Uncomment this test to test your in-place merge sort implementation
+            array = test_array()
+            self.assertEqual(merge_sort(array), sorted(array))
+
+    # Uncomment this test to test your in-place merge sort
     # def test_in_place_merge_sort(self):
-    #     arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
-    #     arr2 = []
-    #     arr3 = [2]
-    #     arr4 = [0, 1, 2, 3, 4, 5]
-    #     arr5 = random.sample(range(200), 50)
 
-    #     self.assertEqual(merge_sort_in_place(arr1, 0, len(arr1)-1), [0,1,2,3,4,5,6,7,8,9])
-    #     self.assertEqual(merge_sort_in_place(arr2, 0, len(arr2)-1), [])
-    #     self.assertEqual(merge_sort_in_place(arr3, 0, len(arr3)-1), [2])
-    #     self.assertEqual(merge_sort_in_place(arr4, 0, len(arr4)-1), [0,1,2,3,4,5])
-    #     self.assertEqual(merge_sort_in_place(arr5, 0, len(arr5)-1), sorted(arr5))
+    #     for test_array in self.test_arrays:
+
+    #         array = test_array()
+    #         self.assertEqual(
+    #             merge_sort_in_place(array, 0, len(array) - 1),
+    #             sorted(array),
+    #         )
 
 
 if __name__ == '__main__':
