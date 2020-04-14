e5be1df4f7e735e34c4b20f3e975f4c3c3c2bc3a
PercivalN <percival.ngan@gmail.com>
2020-04-13 23:11:03 -0500

    Finished bubble sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -17,15 +17,24 @@ def selection_sort( arr ):
 
     return arr
 
+input_array = [2, 54, 11, 1, 145, 8]
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
-    # Start with the first 2 numbers
-    # If the first number is smaller then the second then go onto the next index
-    # Else swap places
-    #   then go onto the next 2 numbers
+    for i in range(0, len(arr)):
+        for index in range(1, len(arr)):
+            current = arr[index]
+            previous = arr[index - 1]
+        
+            if previous <= current:
+                continue
+            else:
+                arr[index] = previous
+                arr[index - 1] = current
     
     return arr
+
+print(bubble_sort(input_array))
 
 
 # STRETCH: implement the Count Sort function below
