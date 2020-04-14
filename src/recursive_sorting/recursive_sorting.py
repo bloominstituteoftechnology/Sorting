ae6e3c82c4f5fbca66cb5ca21999728a2608f032
Karim B <mattiakarim@live.it>
2020-04-14 23:00:50 +0200

    Completed both linear and binary searching algorithms for the stretch goals

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,18 +1,26 @@
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
 def merge( arrA, arrB ):
+    # initial list
     merged_arr = []
+    # initial index for left array
     leftIdx = 0
+    # initial index for right array
     rightIdx = 0
 
     # TO-DO
+    # iterate while both lists have more than one element left
     while(leftIdx < len(arrA) and rightIdx < len(arrB)):
+        # if el in right array is more than el in left 
         if(arrB[rightIdx] > arrA[leftIdx]):
+            # append el in left arr to merged_arr
             merged_arr.append(arrA[leftIdx])
+            # increase by one left arr index to move to next el
             leftIdx += 1
         else:
             merged_arr.append(arrB[rightIdx])
             rightIdx += 1
     
+    # if either array is still unsorted, pass each element till no element is left
     while(leftIdx < len(arrA)):
         merged_arr.append(arrA[leftIdx])
         leftIdx += 1
@@ -27,13 +35,18 @@ def merge( arrA, arrB ):
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
+    # if length of arr is less or equal to 1 return arr
+    # BASE CASE for recursive function
     if(len(arr) <= 1):
         return arr
 
+    # find the middle
     half = len(arr) // 2
+    # divide arr into 2 parts, left and right split by middle using slice
     left = merge_sort(arr[:half])
     right = merge_sort(arr[half:])
 
+    # return the left and right list merged together
     return merge(left, right)
 
 
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -2,20 +2,45 @@
 def linear_search(arr, target):
   
   # TO-DO: add missing code
-
-   return -1   # not found
+  # iterate over the arr
+  for idx in range(len(arr)):
+    # if el at index is uqual to target
+    if arr[idx] == target:
+      # return index
+      return idx
+  # if element is not present, return -1
+  return -1   # not found
 
 
 # STRETCH: write an iterative implementation of Binary Search 
 def binary_search(arr, target):
-
-  if len(arr) == 0:
-    return -1 # array empty
-    
+  # assign a starting and ending point
   low = 0
   high = len(arr)-1
+  # assign a middle
+  mid = len(arr) // 2
+
+  # if the element is not present return -1 
+  if len(arr) == 0:
+    return -1 # array empty
 
   # TO-DO: add missing code
+  # iterate while mid el is not target, and while low index is not equal to high
+  while(arr[mid] != target and low != high):
+    # if target is smaller than el in list at mid point
+    if(target < arr[mid]):
+      # move high to mid - 1
+      high = mid - 1
+      # else move low to mid + 1
+    else:
+      low = mid + 1
+
+    # change middle to new based on array length
+    mid = (low + high) // 2
+
+  # if mid is target el than return index
+  if(arr[mid] == target):
+    return mid
 
   return -1 # not found
 
