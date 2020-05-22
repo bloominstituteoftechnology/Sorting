293a9fbb21e783891a7bcdee81c285c1aa64b6bd
Aaron Shawn Soler <aaronshawnsoler@gmail.com>
2020-05-21 20:05:25 -0400

    sorting algorithms

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,30 +1,41 @@
-# STRETCH: implement Linear Search				
+# STRETCH: implement Linear Search
 def linear_search(arr, target):
-  
-  # TO-DO: add missing code
 
-   return -1   # not found
+    # TO-DO: add missing code
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return arr[i]
+
+    return -1   # not found
 
 
-# STRETCH: write an iterative implementation of Binary Search 
+# STRETCH: write an iterative implementation of Binary Search
 def binary_search(arr, target):
 
-  if len(arr) == 0:
-    return -1 # array empty
-    
-  low = 0
-  high = len(arr)-1
+    if len(arr) == 0:
+        return -1  # array empty
 
-  # TO-DO: add missing code
+    low = 0
+    high = len(arr)-1
 
-  return -1 # not found
+    # TO-DO: add missing code
+    middle = (low + high) // 2
+    while arr[middle] != target:
+        if arr[middle] > target:
+            high = middle
+        elif arr[middle] < target:
+            low = middle
+        elif arr[middle] == target:
+            return arr[middle]
+
+    return -1  # not found
 
 
-# STRETCH: write a recursive implementation of Binary Search 
+# STRETCH: write a recursive implementation of Binary Search
 def binary_search_recursive(arr, target, low, high):
-  
-  middle = (low+high)//2
 
-  if len(arr) == 0:
-    return -1 # array empty
-  # TO-DO: add missing if/else statements, recursive calls
+    middle = (low+high)//2
+
+    if len(arr) == 0:
+        return -1  # array empty
+    # TO-DO: add missing if/else statements, recursive calls
