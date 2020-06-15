8740efc0090c08b5aa8d9e9fbb070fb025ebbec3
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-06-15 22:13:55 +0300

    binary complete

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -10,16 +10,26 @@ def linear_search(arr, target):
 
 
 # STRETCH: write an iterative implementation of Binary Search 
+# The goal of the Binary is to look to an order sequence and determine a value that we looking for
+# is in that sequence or not. Example : [1, 3, 4, 6, 7, 8, 9] the number we are looking for is 7.
+# The way we are doing is to find the mindpoint and comparing with the number we are looking for.
+# If is it higher we choose the items to right or if it is lower we choose the items to the left
+# if we find the number we are searching , we stop the algorithm and return the position to the item
+
 def binary_search(arr, target):
+  left = 0 # we start with the first element 
+  right = len(arr)-1 # and we finish with the last element
 
-  if len(arr) == 0:
-    return -1 # array empty
-    
-  low = 0
-  high = len(arr)-1
-
-  # TO-DO: add missing code
-
+  while left <= right:
+    midpoint = left + (right - left) // 2 # we want the midpoint here so we put 2
+    midpoint_value = arr[midpoint]
+    if midpoint_value == target: # if we found the target equal to the midpoint return as back the position
+      return midpoint
+    elif target < midpoint_value: # the target we are looking for will be at the left
+      right = midpoint -1 # the position will be directly to the left of the midpoint
+    else:
+      left = midpoint + 1 # if the target is greater then we need to check if it is at the right and repositioning
+  
   return -1 # not found
 
 
