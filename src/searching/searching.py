4ef09e24ffbd6f56b137a4026ad76b5aeb0dfe30
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-06-16 10:43:01 +0300

    attempt recursive binary search implementation

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -41,3 +41,11 @@ def binary_search_recursive(arr, target, low, high):
   if len(arr) == 0:
     return -1 # array empty
   # TO-DO: add missing if/else statements, recursive calls
+
+  # if value is smaller than the middle , the it can only be in the left index
+  if arr[middle] > target:
+    return binary_search_recursive(arr, target, low, middle-1)
+  elif arr[middle]<target:
+    return binary_search_recursive(arr, target, middle+1,high)
+  else:
+    return middle
