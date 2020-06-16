36525c7e1d299010c899901a18ad914bf74b9c05
Oluwa-Femi <oluwatolablessing@gmail.com>
2020-06-16 16:25:25 +0100

    Binary sort

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -2,24 +2,33 @@
 def linear_search(arr, target):
   
   # TO-DO: add missing code
+  for i in range(len(arr)):
+        if arr[i] == target:
+            return i
 
-   return -1   # not found
+  return -1   # not found
 
 
 # STRETCH: write an iterative implementation of Binary Search 
 def binary_search(arr, target):
 
-  if len(arr) == 0:
-    return -1 # array empty
-    
-  low = 0
-  high = len(arr)-1
-
-  # TO-DO: add missing code
+  arr.sort()
+  if len(arr) > 0:
+          head = 0
+          tail = len(arr) - 1
+          while head <= tail:
+              mid = int((head + tail) / 2)
+              if target == arr[mid]:
+                  return mid
+              else:
+                  if target < arr[(mid)]:
+                      tail = mid - 1
+                  else:
+                      head = mid + 1
 
   return -1 # not found
 
-
+print(binary_search([1, 2, 4, -5, -7, 8, 9], 8))
 # STRETCH: write a recursive implementation of Binary Search 
 def binary_search_recursive(arr, target, low, high):
   
