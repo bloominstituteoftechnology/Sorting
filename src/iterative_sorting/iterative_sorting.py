3eee184bbef3c530719e84a23285d20ebcff3b58
Oluwa-Femi <oluwatolablessing@gmail.com>
2020-06-16 16:07:08 +0100

    Bubble sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,4 +1,10 @@
 # TO-DO: Complete the selection_sort() function below 
+def swap(arr, index1, index2):
+    item1 = arr[index1]
+    item2 = arr[index2]
+    arr[index2] = item1
+    arr[index1] = item2
+    
 def selection_sort( arr ):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
@@ -6,31 +12,33 @@ def selection_sort( arr ):
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-    for i in range(cur_index + 1, len(arr)):
+        for i in range(cur_index + 1, len(arr)):
             if arr[smallest_index] > arr[i]:
                 smallest_index = i   
 
-
-
         # TO-DO: swap
+        # smallestItem = arr[smallest_index]
+        # itemToSwap = arr[cur_index]
+        # arr[cur_index] = smallestItem
+        # arr[smallest_index] = itemToSwap
+        swap(arr, smallest_index, cur_index)
 
-    smallestItem = arr[smallest_index]
-    itemToSwap = arr[cur_index]
-    arr[cur_index] = smallestItem
-    arr[smallest_index] = itemToSwap
 
 
     return arr
 
     print(selection_sort([17, 18, 2, 4, 1, 7, 19, 19, 3, 10]))
 
-
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
-
+    for i in range(len(arr)):
+        cur_index = i
+        for i in range(cur_index + 1, len(arr)):
+            if arr[cur_index] > arr[i]:
+                swap(arr, cur_index, i)
     return arr
 
-
+print(bubble_sort([17, 18, 2, 4, 1, 7, 19, 19, 3, 10]))
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
 
